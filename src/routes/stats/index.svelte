d594647ad04e0f5c30450d5aae3254d13b4b89e7
ptlthg <edmier12@gmail.com>
2022-08-17 22:02:24 -0500

    Redirect /stats/ to player or home page

---
+++ src/routes/stats/index.svelte
@@ -0,0 +1,19 @@
+<script context="module" lang="ts">
+	import type { AccountInfo, Profiles } from '$lib/skyblock';
+	import type { Load } from './__types/index';
+
+	export const load: Load = async ({ session }) => {
+
+		if (session.user) {
+			return {
+				status: 302,
+				redirect: `/stats/${session.user.ign}`,
+			};
+		}
+
+		return {
+			status: 302,
+			redirect: '/',
+		}
+	}
+</script>
