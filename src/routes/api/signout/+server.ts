d34d3a38ae2909e54bfa3816168dc094ad8e9bb8
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-08-21 16:43:12 -0400

    Simplify logout

--- src/routes/api/signout/+server.ts
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-	
-	const headers = new Headers();
-	headers.append('Set-Cookie', `discord_access_token=deleted; Path=/; Max-Age=-1`)
-	headers.append('Set-Cookie', `discord_refresh_token=deleted; Path=/; Max-Age=-1`)
-
-	return new Response(undefined, { status: 200, headers });
-}
--- src/routes/logout/+page.ts
+++ src/routes/logout/+page.ts
@@ -1,14 +1,23 @@
 import { browser } from '$app/env';
-import { invalidate } from '$app/navigation';
+import { goto, invalidate } from '$app/navigation';
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch }) => {
-	await fetch('/api/signout');
+export const load: PageLoad = async ({ setHeaders }) => {
 	
-	if (browser) { 
+	setHeaders({
+		'Set-Cookie': [
+			`discord_access_token=deleted; Path=/; Max-Age=-1`,
+			`discord_refresh_token=deleted; Path=/; Max-Age=-1`,
+		]
+	})
+
+	if (browser) {
 		await invalidate();
 	}
 
-	throw redirect(302, '/');
+	// Hacky fix for https://github.com/sveltejs/kit/issues/5952
+	if (browser) {
+		return await goto('/');
+	} else throw redirect(302, '/');
 }
