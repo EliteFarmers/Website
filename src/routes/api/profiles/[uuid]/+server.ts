0c70012dbb0966f6b666b509a58f2c2da06fc923
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-08-21 16:41:59 -0400

    Correct responses

--- src/routes/api/profiles/[uuid]/+server.ts
+++ src/routes/api/profiles/[uuid]/+server.ts
@@ -7,16 +7,16 @@ export const GET: RequestHandler = async ({ params }) => {
 	const uuid = params.uuid.replaceAll('-', '');
 
 	if (!uuid || uuid.length !== 32)
-		return new Response(undefined, { status: 400 })
+		return new Response(JSON.stringify({ error: 'Not a valid UUID' }), { status: 400 })
 
 	if (!HYPIXEL_API_KEY) 
-		return new Response(undefined, { status: 500 })
+		return new Response(JSON.stringify({ error: 'API key not provided' }), { status: 500 })
 	
 
 	const profiles = await fetchProfiles(uuid, HYPIXEL_API_KEY);
 
 	if (!profiles) 
-		return new Response(undefined, { status: 404 })
+		return new Response(JSON.stringify({ error: 'Hypixel API couldn\'t be reached.' }), { status: 404 })
 
 	return new Response(JSON.stringify(profiles));
 };
