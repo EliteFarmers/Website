8e14172fac8414744f4cd5f30568595a668703ea
ptlthg <edmier12@gmail.com>
2022-08-22 09:14:48 -0400

    Fix styling

--- src/routes/api/profiles/[uuid]/+server.ts
+++ src/routes/api/profiles/[uuid]/+server.ts
@@ -6,17 +6,19 @@ export const GET: RequestHandler = async ({ params }) => {
 
 	const uuid = params.uuid.replaceAll('-', '');
 
-	if (!uuid || uuid.length !== 32)
-		return new Response(JSON.stringify({ error: 'Not a valid UUID' }), { status: 400 })
+	if (!uuid || uuid.length !== 32) {
+		return new Response(JSON.stringify({ error: 'Not a valid UUID' }), { status: 400 });
+	}
 
-	if (!HYPIXEL_API_KEY) 
-		return new Response(JSON.stringify({ error: 'API key not provided' }), { status: 500 })
-	
+	if (!HYPIXEL_API_KEY) {
+		return new Response(JSON.stringify({ error: 'API key not provided' }), { status: 500 });
+	}
 
 	const profiles = await fetchProfiles(uuid, HYPIXEL_API_KEY);
 
-	if (!profiles) 
-		return new Response(JSON.stringify({ error: 'Hypixel API couldn\'t be reached.' }), { status: 404 })
+	if (!profiles) {
+		return new Response(JSON.stringify({ error: 'Hypixel API couldn\'t be reached.' }), { status: 404 });
+	}
 
 	return new Response(JSON.stringify(profiles));
 };
