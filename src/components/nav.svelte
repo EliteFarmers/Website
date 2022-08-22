839c850fbb9c9c430c6690f7b77c056dca5970d7
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-08-22 18:08:26 -0400

    Use `navigating` store for loading indicators (#10)

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-	import { goto, invalidate } from "$app/navigation";
+	import { goto } from "$app/navigation";
 	import type { DiscordUser } from "$db/models/users";
 	import { page } from "$app/stores";
+	import { navigating } from "$app/stores";
+
+	import { slide } from 'svelte/transition';
+	import { quadInOut, sineInOut } from 'svelte/easing';
 
 	export let discordUser: DiscordUser | false;
 
@@ -18,26 +22,36 @@
 	}
 </script>
 
-<nav class="flex justify-between justify-items-center bg-gray-100 p-2">
-	<section class="flex gap-2 mr-3 justify-start w-1/4">
-		<a class="p-3 hover:bg-gray-50" href="/">EliteDev</a>
-		<a class="bg-gray-200 p-3 rounded-md hover:bg-gray-50" href="/profile">Profile</a>
-	</section>
-	<!-- Section that's centered in the nav bar -->
-	<section class="flex gap-2 mx-6 justify-items-center justify-center w-1/2">
-		<form class="m-0 p-0 w-[100%]" on:submit|preventDefault>
-			<input class="w-5/6 bg-gray-200 p-3 rounded-md hover:bg-gray-50" type="text" placeholder="Search" bind:value={searchVal} />
-			<button class="bg-gray-200 p-3 rounded-md hover:bg-gray-50" on:click={search}>
-				<svg class="h-4 w-4" viewBox="0 0 24 24">
-					<path fill="currentColor" d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-				</svg>
-			</button>
-		</form>
-	</section>
-	<section class="flex gap-2 ml-3 justify-end w-1/4">
-		<!-- Login with discord styled button -->
-		<a sveltekit:reload class="bg-gray-200 p-3 rounded-md hover:bg-gray-50" href="/login" hidden={discordUser !== false}>Login with Discord</a>
-		<!-- Logout button -->
-		<a sveltekit:reload class="bg-gray-200 p-3 rounded-md hover:bg-gray-50" href="/logout" hidden={discordUser === false}>Logout</a>
-	</section>
-</nav>
+<section>
+	<nav class="flex justify-between justify-items-center bg-gray-100 p-2">
+		<section class="flex gap-2 mr-3 justify-start w-1/4">
+			<a class="p-3 hover:bg-gray-50" href="/">EliteDev</a>
+			<a class="bg-gray-200 p-3 rounded-md hover:bg-gray-50" href="/profile">Profile</a>
+		</section>
+		<!-- Section that's centered in the nav bar -->
+		<section class="flex gap-2 mx-6 justify-items-center justify-center w-1/2">
+			<form class="m-0 p-0 w-[100%]" on:submit|preventDefault>
+				<input class="w-5/6 bg-gray-200 p-3 rounded-md hover:bg-gray-50" type="text" placeholder="Search" bind:value={searchVal} disabled={$navigating !== null} />
+				<button class="bg-gray-200 p-3 rounded-md hover:bg-gray-50" on:click={search} disabled={$navigating !== null}>
+					<svg class="h-4 w-4" viewBox="0 0 24 24">
+						<path fill="currentColor" d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+					</svg>
+				</button>
+			</form>
+		</section>
+		<section class="flex gap-2 ml-3 justify-end w-1/4">
+			<!-- Login with discord styled button -->
+			<a sveltekit:reload class="bg-gray-200 p-3 rounded-md hover:bg-gray-50" href="/login" hidden={discordUser !== false}>Login with Discord</a>
+			<!-- Logout button -->
+			<a sveltekit:reload class="bg-gray-200 p-3 rounded-md hover:bg-gray-50" href="/logout" hidden={discordUser === false}>Logout</a>
+		</section>
+	</nav>
+	{#if $navigating}
+		<div class="relative">
+			<div class="w-full h-1 bg-green-300 absolute"
+				transition:slide={{ delay: 100, duration: 500, easing: quadInOut }}
+			></div>
+		</div>
+	{/if}
+</section>
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import Nav from '$comp/nav.svelte';
 	import { page } from '$app/stores';
+	import { navigating } from '$app/stores';
 </script>
 
 <Nav discordUser={$page.data.discordUser} />
+
+{#if $navigating}
+	<!-- Gray out the screen -->
+	<div class="absolute top-0 left-0 w-full h-full bg-gray-100 opacity-50"></div>
+{/if}
 
 <slot />
 
