d2f8dc84a384713995b2114f9fcfffd41b6a58b1
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-08-23 22:42:26 -0400

    PresentCachedFirst (#12)

--- src/routes/api/player/[uuid]/+server.ts
+++ src/routes/api/player/[uuid]/+server.ts
@@ -1,23 +1,26 @@
 import { HYPIXEL_API_KEY } from '$env/static/private'; 
 import { fetchPlayer } from '$lib/data';
+import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ params }) => {
 
 	const uuid = params.uuid.replaceAll('-', '');
 
-	if (!uuid || uuid.length !== 32)
-		return new Response(undefined, { status: 400 })
+	if (!uuid || uuid.length !== 32) {
+		throw error(400, 'Not a valid UUID');
+	}
 
-	if (!HYPIXEL_API_KEY) 
+	if (!HYPIXEL_API_KEY) {
 		return new Response(undefined, { status: 500 })
-	
+	}
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const player = await fetchPlayer(uuid, HYPIXEL_API_KEY) as any;
 
-	if (!player || player.size === 0) 
-		return new Response(undefined, { status: 404 })
-	
+	if (!player || player.size === 0) {
+		throw error(404, 'Hypixel API couldn\'t be reached.');
+	}
+
 	return new Response(JSON.stringify(player));
 };
