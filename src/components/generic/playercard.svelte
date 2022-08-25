373a35062cc45d99ffa45430a389b524e95c2fd3
ptlthg <edmier12@gmail.com>
2022-08-25 11:49:17 -0400

    Simple player card component

---
+++ src/components/generic/playercard.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { User } from "$db/models/users";
+	import { goto } from "$app/navigation";
+	import { PUBLIC_HOST_URL } from "$env/static/public";
+
+	export let user: Partial<User>;
+</script>
+
+<section class="bg-gray-100 rounded-lg p-2 flex items-center justify-center flex-col" on:click={() => goto(`${PUBLIC_HOST_URL}/stats/${user.ign}`)}>
+	<img class="w-24 h-24" src={`https://mc-heads.net/head/${user.uuid}`} alt={`${user.ign}'s player head.`}>
+	<h1 class="text-center text-sm p-1">{user.ign}</h1>
+</section>
