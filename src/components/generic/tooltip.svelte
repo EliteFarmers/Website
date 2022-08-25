6500f348f78f6317384d220b748305ad2b8bdc99
ptlthg <edmier12@gmail.com>
2022-08-25 00:09:31 -0400

    Tooltip on hover component

---
+++ src/components/generic/tooltip.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let text: string; 
+	let show = false;
+</script>
+
+<section on:mouseenter={() => show = true} on:mouseleave={() => show = false}>
+	<slot />
+	<div class="absolute" hidden={!show}>
+		<div class="bg-gray-300 rounded-lg p-3 top-2">
+			{text}
+		</div>
+	</div>
+</section>
