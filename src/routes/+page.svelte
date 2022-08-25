5b265ea01c5da376feb728baa5f63acd88ba3ab1
Kaeso <edmier12@gmail.com>
2022-08-25 12:01:03 -0400

    PopularPlayers (#17)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
+	import type { User } from "$db/models/users";
 	import { goto } from "$app/navigation";
+	import { PUBLIC_HOST_URL } from "$env/static/public";
+	import { onMount } from "svelte";
+
+	import PlayerCard from "$comp/generic/playercard.svelte";
 
 	let enteredText = '';
+	let topViewed: Partial<User>[] = [];
+
+	onMount(async () => {
+		const viewed = await fetch(`${PUBLIC_HOST_URL}/api/leaderboard/views`);
+		const json = await viewed.json();
+
+		if (viewed.status === 200) {
+			topViewed = json;
+		}
+	});
 </script>
 
 <svelte:head>
@@ -14,7 +29,7 @@
 
 	<div class="flex align-items-center justify-center justify-self-center relative">
 		<form on:submit|preventDefault class="w-10/12 flex align-items-center justify-center">
-			<div class="relative inline-block w-1/3">
+			<div class="relative inline-block md:w-1/3">
 				<input class="p-2 m-4 mb-0 text-left border-2 rounded-lg w-[100%] mx-auto" 
 					bind:value={enteredText} maxlength="100" placeholder="Search for player" type="text"
 				/>
@@ -27,5 +42,13 @@
 			</button>
 		</form>
 	</div>
+
+	<section class="flex justify-center mt-10">
+		<div class="grid gap-2 grid-cols-2 md:grid-cols-5 justify-center w-[90%]">
+			{#if topViewed.length > 0} {#each topViewed as player}
+				<PlayerCard user={player} />
+			{/each} {/if}
+		</div>
+	</section>
 </main>
 
