cab4d70a804e90789e97087b89b098f22235589d
ptlthg <edmier12@gmail.com>
2022-08-31 17:37:29 -0400

    Better responsivity with breakpoints

--- src/components/generic/dropdown.svelte
+++ src/components/generic/dropdown.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { slide } from 'svelte/transition';
 	import { quadInOut } from 'svelte/easing';
- 
+
+	export let hasItems = true;
 	let collapse = true;
 
 </script>
@@ -9,7 +10,7 @@
 <section class="mx-1 relative cursor-pointer" on:mouseleave={() => collapse = true} on:mouseenter={() => collapse = false}>
 	<div class="p-3 bg-gray-200 rounded-lg">
 		<slot name="top" />	
-		{#if !collapse}
+		{#if (!collapse && hasItems)}
 			<div transition:slide={{ duration: 500, easing: quadInOut }} class="absolute bg-gray-200 rounded-lg p-3 left-0">
 				<slot name="rest" />
 			</div>
