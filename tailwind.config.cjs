c61d64b19cb3b6fd2eb55a7eb121939e03fe201d
ptlthg <edmier12@gmail.com>
2022-09-02 14:42:11 -0400

    Add new `xs` breakpoint and responsive text sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
 	mode: 'jit',
-	content: ['./src/**/*.svelte']
+	content: ['./src/**/*.svelte'],
+	theme: {
+		extend: {
+			fontSize: {
+				'body-sm': '0.6rem',
+				'body': '0.875rem',
+				'body-lg': '1rem',
+				'body-xl': '1.5rem',
+			},
+		},
+		screens: {
+			'xs': '320px',
+			...defaultTheme.screens,
+		},
+	}
 };
