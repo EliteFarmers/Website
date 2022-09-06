9e90bbeee394789ac513c54d6d6e93e6b6e01b0d
ptlthg <edmier12@gmail.com>
2022-09-05 23:46:55 -0400

    Weight per crop values

---
+++ src/lib/constants/weights.ts
@@ -0,0 +1,12 @@
+export const CROPS_PER_ONE_WEIGHT = {
+	wheat: 100_000,
+	carrot: 300_000,
+	potato: 300_000,
+	sugarcane: 200_000,
+	netherwart: 250_000,
+	pumpkin: 70_877.248,
+	melon: 354_386.238,
+	mushroom: 72_000,
+	cocoa: 219_648.785,
+	cactus: 159_297.912,
+}
