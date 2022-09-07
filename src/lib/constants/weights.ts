60e8c50c02df4b6706a75fa332c681bfb1d84b8c
ptlthg <edmier12@gmail.com>
2022-09-07 15:01:43 -0400

    Remove magic numbers

--- src/lib/constants/weights.ts
+++ src/lib/constants/weights.ts
@@ -10,3 +10,15 @@ export const CROPS_PER_ONE_WEIGHT = {
 	cocoa: 219_648.785,
 	cactus: 159_297.912,
 }
+
+// Bonus
+
+// Rewards for farming level
+export const FARMING_LEVEL_50_BONUS = 100;
+export const FARMING_LEVEL_60_BONUS = 250;
+// For every gold medal <= max, add 0.5 to the weight
+export const MAX_MEDAL_BONUS = 1000;
+export const WEIGHT_PER_GOLD_MEDAL = 0.5;
+// Get 5 bonus weight for every tier 12 farming minion
+export const MINION_REWARD_AT_TIER = 12;
+export const MINION_REWARD_WEIGHT = 5;
