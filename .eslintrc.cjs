781bd98d808afcdb8dc35c54b4c3e43027bc53d1
ptlthg <edmier12@gmail.com>
2022-10-03 16:54:09 -0400

    Fix config files throwing errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,12 +8,6 @@ module.exports = {
 		'plugin:@typescript-eslint/strict',
 		'prettier',
 	],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs', 'svelte.config.js', 'vite.config.js'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript'),
-	},
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
