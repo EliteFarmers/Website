252a3d6785d54b2b42096a5345568da77669a631
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-10-03 19:07:14 -0400

    Add lint action (#25)
    Add automatic GitHub action for linting.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,17 +8,10 @@ module.exports = {
 		'plugin:@typescript-eslint/strict',
 		'prettier',
 	],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs', 'svelte.config.js', 'vite.config.js'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript'),
-	},
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
-		tsconfigRootDir: __dirname,
-		project: ['./tsconfig.json'],
+		project: ['tsconfig.json'],
 	},
 	env: {
 		browser: true,
