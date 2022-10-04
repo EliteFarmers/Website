35d8c627f8889867ed35636947a47db66a847797
ptlthg <edmier12@gmail.com>
2022-10-03 21:39:14 -0400

    Refetch weight in info endpoint

--- src/routes/api/info/[uuid]/+server.ts
+++ src/routes/api/info/[uuid]/+server.ts
@@ -1,8 +1,10 @@
 import { GetUser } from '$db/database';
 import type { RequestHandler } from './$types';
+import { GET as fetchWeight } from '../../weight/[uuid]/+server';
+import type { UserInfo } from '$db/models/users';
 
-export const GET: RequestHandler = async ({ params }) => {
-	const uuid = params.uuid.replaceAll('-', '');
+export const GET: RequestHandler = async (event) => {
+	const uuid = event.params.uuid.replaceAll('-', '');
 
 	if (!uuid || uuid.length !== 32) {
 		return new Response(JSON.stringify({ error: 'Not a valid UUID' }), { status: 400 });
@@ -16,6 +18,16 @@ export const GET: RequestHandler = async ({ params }) => {
 
 	if (!user.info) {
 		return new Response(JSON.stringify({ error: 'User has no info' }), { status: 404 });
+	}
+
+	const info = user.info as Partial<UserInfo>;
+
+	if (info.profiles) {
+		const response = await fetchWeight(event);
+
+		if (response.status === 200) {
+			return response;
+		}
 	}
 
 	return new Response(JSON.stringify(user.info));
