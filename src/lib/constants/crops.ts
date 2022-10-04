80a05406f6210d7e7f668b8e0e74b58a9ed2a457
ptlthg <edmier12@gmail.com>
2022-10-03 22:13:15 -0400

    Constants for crop collections/names

---
+++ src/lib/constants/crops.ts
@@ -0,0 +1,25 @@
+export const PROPER_CROP_NAME: Partial<Record<string, string>> = {
+	CACTUS: 'Cactus',
+	CARROT_ITEM: 'Carrot',
+	'INK_SACK:3': 'Cocoa',
+	MELON: 'Melon',
+	MUSHROOM_COLLECTION: 'Mushroom',
+	NETHER_STALK: 'Nether Wart',
+	POTATO_ITEM: 'Potato',
+	PUMPKIN: 'Pumpkin',
+	SUGAR_CANE: 'Sugar Cane',
+	WHEAT: 'Wheat',
+};
+
+export const CROPS_MAX_TIER: Partial<Record<string, number>> = {
+	Cactus: 9,
+	Carrot: 9,
+	Cocoa: 9,
+	Melon: 9,
+	Mushroom: 9,
+	'Nether Wart': 12,
+	Potato: 9,
+	Pumpkin: 11,
+	'Sugar Cane': 9,
+	Wheat: 11,
+};
