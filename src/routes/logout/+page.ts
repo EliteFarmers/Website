c6b71c58a47faa88f11e59a00472b5b480dbfd78
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-10-05 15:09:53 -0400

    Update Dependencies (#42)
    * Update dependencies
    
    * Rename hooks.ts to new server side only name
    
    * Move clearing login cookies to server side load.

--- src/routes/logout/+page.ts
+++ src/routes/logout/+page.ts
@@ -3,13 +3,8 @@ import { invalidateAll } from '$app/navigation';
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ setHeaders }) => {
-	setHeaders({
-		'Set-Cookie': [
-			`discord_access_token=deleted; Path=/; Max-Age=-1`,
-			`discord_refresh_token=deleted; Path=/; Max-Age=-1`,
-		],
-	});
+export const load: PageLoad = async ({ parent }) => {
+	await parent();
 
 	if (browser) {
 		await invalidateAll();
