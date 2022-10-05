c63cc9eef3744c639e665fa9cca0ed900efed8d9
ptlthg <edmier12@gmail.com>
2022-10-05 15:06:41 -0400

    Move clearing login cookies to server side load.

---
+++ src/routes/logout/+page.server.ts
@@ -0,0 +1,18 @@
+import type { ServerLoad } from "@sveltejs/kit";	
+
+export const load: ServerLoad = ({ cookies }) => {
+	
+	cookies.set('discord_access_token', 'deleted;', {
+		path: '/',
+		maxAge: -1,
+	});
+
+	cookies.set('discord_refresh_token', 'deleted;', {
+		path: '/',
+		maxAge: -1,
+	});
+
+	return {
+		status: 200,
+	};
+};
