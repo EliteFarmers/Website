99562062f9f9775be5b6329984c85649d9800a4b
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-10-15 21:01:21 -0400

    Pass in minion tier field

--- src/components/stats/collections.svelte
+++ src/components/stats/collections.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { WeightInfo } from '$db/models/users';
-	import { PROPER_CROP_NAME } from '$lib/constants/crops';
+	import { PROPER_CROP_NAME, PROPER_CROP_TO_MINION } from '$lib/constants/crops';
 	import type { ProfileMember } from '$lib/skyblock';
 	import CollectionBar from './collectionbar.svelte';
 
@@ -9,12 +9,19 @@
 
 	const collections = Object.entries(member.collection ?? {})
 		.filter(([key]) => PROPER_CROP_NAME[key])
-		.map(([key, value]) => ({ name: PROPER_CROP_NAME[key], value }));
+		.map(([key, value]) => ({ name: PROPER_CROP_NAME[key], value, minionTierField: 0 }))
+		.sort((a, b) => a.name?.localeCompare(b.name ?? '') ?? 0);
+
+	for (const collection of collections) {
+		if (!collection.name) continue;
+		const minion = PROPER_CROP_TO_MINION[collection.name] ?? 'no';
+		collection.minionTierField = member.minions[minion] ?? 0;
+	}
 </script>
 
 <section class="py-4">
 	<div class="flex justify-center align-middle">
-		<div class="w-[90%]">
+		<div class="md:w-[70%]">
 			{#each collections as item}
 				<CollectionBar {...item} weight={weight?.farming?.sources?.[item.name ?? ''] ?? 0} />
 			{/each}
