986f548f7361266c5d7010129e41998ef0117409
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-10-15 21:00:42 -0400

    Minion component

---
+++ src/components/stats/minion.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import Tooltip from '$comp/generic/tooltip.svelte';
+
+	export let name: string;
+	export let index: number;
+	export let tierField: number;
+
+	// Turn tierField into its binary representation
+	const tiers = tierField.toString(2).substring(0, 12).split('');
+</script>
+
+<div class="image-container p-1 md:p-2 lg:p-3">
+	<Tooltip>
+		<div class="text-body">{name} Minion</div>
+		<div class="flex gap-1">
+			{#each tiers as tier, i}
+				<div class="flex align-middle text-center justify-center">
+					<div class="w-4 h-6 mx-0 text-body" style="background-color: {tier === '1' ? '#22c55e' : 'gray'};">
+						{i + 1}
+					</div>
+				</div>
+			{/each}
+		</div>
+	</Tooltip>
+	<div class="image" style="background-position: 100% {1000 - 100 * index}%;" />
+	<div class="tier-border">
+		{#each tiers as tier, i}
+			<div class="tier" style="background-color: {tier === '1' ? '#22c55e' : 'lightgray'}; grid-area: a{i};" />
+		{/each}
+	</div>
+	<div class="tier-cover" />
+</div>
+
+<style lang="postcss">
+	.image-container {
+		@apply flex align-middle justify-center bg-gray-100 rounded-lg;
+		width: 6vw !important;
+		height: 6vw;
+		aspect-ratio: 1 / 1;
+		position: relative;
+	}
+
+	.tier-border {
+		@apply grid absolute rounded-lg w-full h-full top-0 left-0;
+		gap: 0.25vw;
+		grid-template-areas:
+			'a10 a11 a0 a1'
+			'a9 . . a2'
+			'a8 . . a3'
+			'a7 a6 a5 a4';
+		z-index: 1;
+		overflow: hidden;
+	}
+
+	.tier-cover {
+		@apply absolute bg-gray-100;
+		border-radius: 0.25rem;
+		width: 86%;
+		height: 86%;
+		top: 7%;
+		left: 7%;
+		z-index: 2;
+	}
+
+	.image {
+		@apply rounded-lg inline-block w-full aspect-square;
+		background-image: url(/images/cropatlas.png);
+		background-size: 200% 1000%;
+		z-index: 3;
+	}
+</style>
