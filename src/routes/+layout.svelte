ef3aa7d8a6e5765cd1a2b76281359542f5e07c0f
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-10-17 12:51:37 -0400

    Use dark/light class above slot

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,47 @@
 	import Nav from '$comp/nav.svelte';
 	import { page } from '$app/stores';
 	import { navigating } from '$app/stores';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+
+	enum Theme {
+		Light = 'light',
+		Dark = 'dark',
+		Unset = 'unset',
+	};
+
+	let theme: Theme = Theme.Unset;
+	if (browser) {
+		const stored = localStorage.getItem('theme') ?? Theme.Unset;
+		theme = stored as Theme;
+	}
+
+	onMount(async () => {
+		const stored = localStorage.getItem('theme') ?? Theme.Unset;
+		
+		if (stored === Theme.Unset) {
+			const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+			if (prefersDark.matches) {
+				theme = Theme.Dark;
+			} else {
+				theme = Theme.Light;
+			}
+		}
+	});
 </script>
 
-<Nav discordUser={$page.data.discordUser} />
+<div class="{theme}">
+	<Nav discordUser={$page.data.discordUser} />
 
-{#if $navigating}
-	<!-- Gray out the screen -->
-	<div class="absolute top-0 left-0 w-full h-full bg-gray-100 opacity-50" />
-{/if}
+	{#if $navigating}
+		<!-- Gray out the screen -->
+		<div class="absolute top-0 left-0 w-full h-full bg-gray-100 opacity-50" />
+	{/if}
 
-<slot />
+	<div class="dark:bg-zinc-900">
+		<slot />
+	</div>
+</div>
 
 <style lang="postcss">
 	@tailwind base;
@@ -19,6 +50,15 @@
 	@tailwind utilities;
 
 	:global(body) {
+		background-color: rgb(24 24 27 / 1);
 		margin: 0;
+	}
+
+	.dark {
+		@apply bg-zinc-900 text-white;
+	}
+
+	.light {
+		@apply bg-white;
 	}
 </style>
