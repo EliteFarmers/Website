4e8303a6f7d55292a67a7bf47bdfcc3f489784b9
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-10-26 12:16:52 -0400

    Add bot invite link and server links to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import type { User } from '$db/models/users';
 	import { goto } from '$app/navigation';
-	import { PUBLIC_HOST_URL } from '$env/static/public';
+	import { PUBLIC_HOST_URL, PUBLIC_BOT_INVITE, PUBLIC_SUPPORT_SERVER_INVITE, PUBLIC_COMMUNITY_INVITE } from '$env/static/public';
 	import { onMount } from 'svelte';
 
 	import PlayerCard from '$comp/generic/playercard.svelte';
+	import TwoPanel from '$comp/generic/twopanel.svelte';
 
 	let enteredText = '';
 	let topViewed: Partial<User>[] = [];
@@ -23,11 +24,11 @@
 	<title>Elite</title>
 </svelte:head>
 
-<main class="">
+<main>
 	<h1 class="text-4xl text-center my-16">Welcome to Elite!</h1>
 	<p class="text-body-xl text-center">Look up any skyblock player!</p>
 
-	<div class="flex align-items-center justify-center justify-self-center relative">
+	<div class="flex align-items-center justify-center justify-self-center relative mb-8">
 		<form on:submit|preventDefault class="w-10/12 flex align-items-center justify-center">
 			<div class="relative inline-block md:w-1/3">
 				<input
@@ -48,6 +49,46 @@
 			</button>
 		</form>
 	</div>
+
+	<TwoPanel>
+		<div slot="left" class="m-4 p-4 w-full rounded-lg bg-gray-100 dark:bg-zinc-800">
+			<h1 class="p-2 mb-4 w-full text-center bg-gray-200 dark:bg-zinc-700 rounded-md text-xl">
+				Join The Discord
+			</h1>
+			<p class="w-full text-center mb-4">
+				Join an exclusive community of Elite Farmers! Full membership only unlocked after reaching 2,500 farming
+				weight. For website/bot support, join the support server!
+			</p>
+			<div class="flex flex-row justify-evenly">
+				<a
+					href={PUBLIC_SUPPORT_SERVER_INVITE}
+					target="_blank"
+					class="text-center px-4 py-2 m-2 rounded-md bg-gray-200 hover:bg-gray-400 dark:bg-zinc-700 dark:hover:bg-zinc-900"
+					>Join Support Server</a
+				>
+				<a
+					href={PUBLIC_COMMUNITY_INVITE}
+					target="_blank"
+					class="text-center px-4 py-2 m-2 rounded-md bg-green-400 hover:bg-green-500 dark:bg-green-500 dark:hover:bg-green-700"
+					>Join Elite Farmers</a
+				>
+			</div>
+		</div>
+		<div slot="right" class="m-4 p-4 w-full flex flex-col justify-between rounded-lg bg-gray-100 dark:bg-zinc-800">
+			<h1 class="p-2 mb-4 w-full text-center bg-gray-200 dark:bg-zinc-700 rounded-md text-xl">
+				Add To Your Server
+			</h1>
+			<p class="w-full text-center">Quickly access stats and leaderboard in Discord! Elite Bot is verified and already present in more than 100 servers!</p>
+			<div class="flex justify-center">
+				<a
+					href={PUBLIC_BOT_INVITE}
+					target="_blank"
+					class="text-center px-4 py-2 m-2 rounded-md bg-blue-400 hover:bg-blue-500 dark:bg-blue-500 dark:hover:bg-blue-700"
+					>Invite Elite Bot</a
+				>
+			</div>
+		</div>
+	</TwoPanel>
 
 	<section class="flex justify-center mt-10">
 		<div class="grid gap-2 grid-cols-2 md:grid-cols-5 justify-center w-[90%]">
