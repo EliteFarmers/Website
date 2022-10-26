1c071c0cebacb7a4aeec1d6884363f2f6ec95cfb
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-10-26 12:16:23 -0400

    Generic two panel component

---
+++ src/components/generic/twopanel.svelte
@@ -0,0 +1,6 @@
+<section class="flex w-full justify-center">
+	<div class="flex flex-col md:flex-row w-10/12 md:w-3/4 justify-center">
+		<slot name="left" />
+		<slot name="right" />
+	</div>
+</section>
