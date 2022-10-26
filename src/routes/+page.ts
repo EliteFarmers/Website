2539b9e551a2492a8f42b522018d9518686f32a7
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-10-26 15:20:04 -0400

    Include leaderboard in page data

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,16 @@
+import type { LeaderboardEntry } from '$db/database';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = () => undefined;
+export const load: PageLoad = async ({ fetch, setHeaders }) => {
+	const lbFetch = await fetch('/api/leaderboard/weight?limit=10');
+	const lb = (await lbFetch.json()) as LeaderboardEntry[];
+
+	setHeaders({
+		// Max age of 5 minutes
+		'Cache-Control': 'max-age=300',
+	});
+
+	return {
+		lb: lb,
+	};
+};
