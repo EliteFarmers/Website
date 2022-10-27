23021fa99bd40185f7c5d4df2005b27a00570e0e
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-10-26 23:36:13 -0400

    Info page and tables

---
+++ src/routes/info/bonustable.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import { FARMING_LEVEL_50_BONUS, FARMING_LEVEL_60_BONUS, MAX_MEDAL_BONUS, MINION_REWARD_AT_TIER, MINION_REWARD_WEIGHT } from '$lib/constants/weights';
+</script>
+
+<table class="w-full">
+	<thead>
+		<tr class="bg-gray-200 dark:bg-zinc-700">
+			<th class="text-left">Source</th>
+			<th class="text-left">Notes</th>
+			<th class="text-left">Reward</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr class="bg-gray-100 dark:bg-zinc-800">
+			<td>Farming 50</td>
+			<td>One time reward obtained upon reaching farming level 50.</td>
+			<td>{FARMING_LEVEL_50_BONUS}</td>
+		</tr>
+		<tr class="bg-gray-100 dark:bg-zinc-800">
+			<td>Farming 60</td>
+			<td>Rewarded after reaching both the XP required and unlocking the level cap.</td>
+			<td>{FARMING_LEVEL_60_BONUS}</td>
+		</tr>
+		<tr class="bg-gray-100 dark:bg-zinc-800">
+			<td>Anita Bonus</td>
+			<td>For each level of Anita Bonus unlocked, you'll get 2 more weight.</td>
+			<td>2 (Max 30)</td>
+		</tr>
+		<tr class="bg-gray-100 dark:bg-zinc-800">
+			<td>Gold Medals</td>
+			<td>Each 50 claimed gold medals earns you 25 weight each, up to {MAX_MEDAL_BONUS} medals.</td>
+			<td>25 (Max 500)</td>
+		</tr>
+		<tr class="bg-gray-100 dark:bg-zinc-800">
+			<td>Farming Minions</td>
+			<td>For each unlocked tier {MINION_REWARD_AT_TIER} farming minion, you'll earn {MINION_REWARD_WEIGHT} bonus weight.</td>
+			<td>{MINION_REWARD_WEIGHT} (Max 50)</td>
+		</tr>
+	</tbody>
+</table>
+
+<style lang="postcss">
+	td, th {
+		@apply p-1 px-2;
+	}
+
+	tbody > tr:nth-child(odd) {
+		@apply bg-opacity-50;
+	}
+</style>
