a8f49dc5c6b5d37d1fd41c69744515b9b84b6409
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-10-31 18:11:14 -0400

    Fix duplicate paragraph

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -90,11 +90,11 @@
 		<article class="w-10/12 md:w-1/2">
 			<h2 class="text-2xl my-8">Why isn't farming XP counted?</h2>
 			<p class="text-lg my-4">
-				Yes! Hypixel seems to be counting the amount of farmed crops accurately, so the crop collections are
-				accurate. However, that was not always the case. In the past, Hypixel has had issues with counting
-				farmed crops when a player was in a shared island with another player. This has since been fixed as of
-				November 2nd, 2021. Sadly, there is no way to add back the crops that were lost during this time as the
-				amount lost differed wildly from player to player.
+				Farming XP is not counted because it is not a good representation of how much a player has farmed. 
+				Outside of one time bonus weight amounts (see above table), farming xp isn't considered. This is 
+				to ensure that players who farm using rabbit or who farm higher XP crops don't have an unfair advantage
+				over players who farm using other methods. Farming Weight is meant to balance the skill and time taken 
+				to farm each crop.
 			</p>
 		</article>
 		<article class="w-10/12 md:w-1/2">
