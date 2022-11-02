e6a97d4b2feaf900180132550030e2970b929a30
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-11-02 01:06:02 -0400

    Add crop colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,6 +12,18 @@ module.exports = {
 				'body-lg': '1rem',
 				'body-xl': '1.5rem',
 			},
+			colors: {
+				wheat: '#d5da45',
+				melon: '#bb170b',
+				cactus: '#3b5b1d',
+				pumpkin: '#a0560b',
+				carrot: '#ff8e09',
+				potato: '#e9ba62',
+				sugarcane: '#82a859',
+				netherwart: '#5c151a',
+				mushroom: '#725643',
+				cocoa: '#61381d',
+			},
 		},
 		screens: {
 			xs: '320px',
