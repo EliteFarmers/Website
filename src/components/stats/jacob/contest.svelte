b6c5ac618c227cf70cd19498747d46c546890b9d
Kaeso <24925519+ptlthg@users.noreply.github.com>
2022-11-02 23:24:11 -0400

    More Jacob (#89)
    * Fix lb entry spacing
    
    * Medals on contests
    
    * Add unclaimed text
    
    * General Jacob's stats component
    
    * Add ign and stats to Jacob component

--- src/components/stats/jacob/contest.svelte
+++ src/components/stats/jacob/contest.svelte
@@ -5,18 +5,31 @@
 	type JacobContestCrop = JacobContest & { crop?: string };
 
 	export let contest: JacobContestCrop;
+	const { crop, position, participants, collected, timestamp } = contest;
 
-	const cropName = (contest.crop ? PROPER_CROP_NAME[contest.crop] : undefined) ?? 'Not Found';
-	const ranking = contest.position !== undefined ? `#${contest.position + 1} / ${contest.participants}` : `Unclaimed`;
+	const cropName = (crop ? PROPER_CROP_NAME[crop] : undefined) ?? 'Not Found';
+	const ranking = position !== undefined;
+
+	let medal = '';
+	if (position !== undefined && participants) {
+		if (position <= participants * 0.05 + 1) {
+			medal = '🥇 ';
+		} else if (position <= participants * 0.25 + 1) {
+			medal = '🥈 ';
+		} else if (position <= participants * 0.6 + 1) {
+			medal = '🥉 ';
+		}
+	}
 </script>
 
-<div class="p-2 flex flex-col gap-0.5 rounded-md bg-gray-200 dark:bg-zinc-700 border-l-4 {contest.crop}">
+<div class="p-2 flex flex-col gap-0.5 rounded-md bg-gray-200 dark:bg-zinc-700 border-l-4 {crop}">
 	<h3 class="first-letter:uppercase text-sm">
 		<span class="p-0.5 px-1.5 bg-gray-100 dark:bg-zinc-800 rounded-md">{cropName}</span>
-		<span class="text-sm font-semibold">{ranking}</span>
+		<span class="text-sm font-semibold">{ranking ? `#${position + 1}` : 'Unclaimed'}</span>
+		<span class="text-xs">{ranking ? `/ ${participants}` : ''}</span>
 	</h3>
-	<h3 class="text-lg font-semibold">{contest.collected.toLocaleString()}</h3>
-	<h6 class="text-xs font-mono font-semibold">{getReadableSkyblockDate(contest.timestamp)}</h6>
+	<h3 class="text-lg font-semibold">{medal}{collected.toLocaleString()}</h3>
+	<h6 class="text-xs font-mono font-semibold">{getReadableSkyblockDate(timestamp)}</h6>
 </div>
 
 <style lang="postcss">
--- src/components/stats/jacob/jacobinfo.svelte
+++ src/components/stats/jacob/jacobinfo.svelte
@@ -2,16 +2,19 @@
 	import type { JacobData } from '$lib/skyblock';
 	import Medals from './medals.svelte';
 	import Recents from './recents.svelte';
-	import RecentContests from './recents.svelte';
+	import Stats from './stats.svelte';
 
 	export let jacob: JacobData;
+	export let ign: string;
 </script>
 
 <section class="py-4 flex justify-center align-middle" aria-labelledby="Jacob">
 	<div class="w-[90%] md:w-[70%] bg-gray-100 dark:bg-zinc-800 rounded-lg p-4">
-		<h1 id="Jacob" class="text-3xl text-center pt-2">Jacob's Stats</h1>
+		<h1 id="Jacob" class="text-3xl text-center pt-2">{ign} | Jacob Stats</h1>
 
 		<Medals total={jacob.medals} earned={jacob.earned_medals} />
+
+		<Stats {jacob} />
 
 		<Recents contests={jacob.contests} />
 	</div>
