57f93c147bb578ce501028688ec3842dff3bad38
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-11-09 12:35:52 -0500

    Add `/account/discord/[id]` endpoint

---
+++ src/routes/api/account/discord/[id]/+server.ts
@@ -0,0 +1,18 @@
+import { GetAccountFromDiscord } from '$db/database';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ params }) => {
+	const { id } = params as { id: string };
+
+	if (!id || id.length < 17 || !/^\d+$/.test(id)) {
+		return new Response('Invalid Discord ID', { status: 400 });
+	}
+
+	const account = await GetAccountFromDiscord(id);
+
+	if (!account || !account.success) {
+		return new Response(JSON.stringify({ success: false, error: 'Account not found' }), { status: 404 });
+	}
+
+	return new Response(JSON.stringify(account));
+};
