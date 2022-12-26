e80e2c7acccee6d135b3a9faf21d3641597b84c5
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2022-12-26 16:45:44 -0600

    Fix reponsitivity

--- src/components/stats/collections.svelte
+++ src/components/stats/collections.svelte
@@ -26,7 +26,7 @@
 
 <section class="py-4">
 	<div class="flex justify-center align-middle">
-		<div class="w-[90%] md:w-[70%]">
+		<div class="w-[90%] lg:w-[70%]">
 			<button
 				class="ml-2 -mt-4 py-1 rounded-md w-20 bg-gray-100 dark:bg-zinc-800 whitespace-nowrap text-sm hover:font-semibold"
 				on:click={swap}>{weightSort ? 'Weight ↓' : 'A-Z ↓'}</button
