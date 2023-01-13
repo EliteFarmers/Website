74c3391ad90cda58de74009e5db5f4e101601e65
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-01-12 18:55:31 -0600

    Fix formatting

--- src/components/stats/player/skyblocklevel.svelte
+++ src/components/stats/player/skyblocklevel.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import Tooltip from "$comp/generic/tooltip.svelte";
-	import { SKYBLOCK_LEVEL_COLORS } from "$lib/constants/levels";
+	import Tooltip from '$comp/generic/tooltip.svelte';
+	import { SKYBLOCK_LEVEL_COLORS } from '$lib/constants/levels';
 
 	export let xp: number;
 
@@ -10,7 +10,7 @@
 <div
 	class="relative max-w-fit p-2 px-3 m-1 text-lg bg-gray-200 dark:bg-zinc-700 rounded-md"
 	aria-label="Skyblock Level"
-	>
+>
 	<span class="font-mono font-bold" style="color: {color};">{Math.floor(xp / 100)}</span>
 	<Tooltip>
 		<div class="flex flex-col gap-1">
