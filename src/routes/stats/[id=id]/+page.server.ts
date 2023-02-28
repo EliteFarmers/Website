8a1102e1780e4acf0da4b7df39f429cb818868b2
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-02-28 17:25:56 -0500

    Add similar name results to search

--- src/routes/stats/[id=id]/+page.server.ts
+++ src/routes/stats/[id=id]/+page.server.ts
@@ -1,4 +1,4 @@
-import { error, redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import type { ProfileData } from '$lib/skyblock';
 import type { PageServerLoad } from './$types';
 import { accountFromId } from '$lib/data';
@@ -9,7 +9,7 @@ export const load: PageServerLoad = async ({ params, fetch }) => {
 	const account = await accountFromId(id);
 
 	if (!account) {
-		throw error(404, 'Minecraft account not found');
+		throw redirect(303, `/search/${id}`);
 	}
 
 	const { id: uuid, name: ign } = account.account;
@@ -17,13 +17,13 @@ export const load: PageServerLoad = async ({ params, fetch }) => {
 	const response = await fetch(`/api/profiles/${uuid}/selected`);
 
 	if (!response.ok) {
-		throw error(404, 'Skyblock profile not found for this player!');
+		throw redirect(303, `/search/${id}`);
 	}
 
 	const data = (await response.json()) as { profile: ProfileData; success: boolean };
 
 	if (!data.success) {
-		throw error(404, 'Skyblock profile not found for this player!');
+		throw redirect(303, `/search/${id}`);
 	}
 
 	throw redirect(303, `/stats/${ign}/${data.profile.profile_id}`);
