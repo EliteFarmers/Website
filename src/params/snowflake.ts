d207d3badb47b92feb6595b91ecd3e70c834df69
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-03-10 11:31:46 -0500

    Export param matcher names

--- src/params/snowflake.ts
+++ src/params/snowflake.ts
@@ -4,3 +4,5 @@ export const match = ((param) => {
 	//* Snowflakes only contain numbers 
 	return param.length >= 18 && /^[0-9]+$/.test(param);
 }) satisfies ParamMatcher;
+
+export const IsSnowflake = match;
