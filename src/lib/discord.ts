14b391199d704aae44bb3b2a4e331ff4f9bdb50c
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-03-10 11:32:45 -0500

    Further work on server displays

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -1,6 +1,6 @@
 
 export async function FetchGuilds(accessToken: string) {
-	const url = 'https://discord.com/api/users/@me/guilds';
+	const url = 'https://discord.com/api/v10/users/@me/guilds';
 
 	const response = await fetch(url, {
 		headers: {
@@ -8,7 +8,7 @@ export async function FetchGuilds(accessToken: string) {
 		},
 	});
 
-	const json = await response.json();
+	const json = await response.json() as unknown;
 
 	if (response.status !== 200) {
 		return undefined;
@@ -20,17 +20,24 @@ export async function FetchGuilds(accessToken: string) {
 
 	// Filter out guilds that the user doesn't have MANAGE_GUILD permissions in
 	// Sort so that the guilds are in alphabetical order but the user's guild is first
-	const guilds = json.filter((guild: Guild) => guild.permissions & 0x20 || guild.owner).sort((a: Guild, b: Guild) => {
+	const guilds = json.filter(CanEditGuild).sort((a: Guild, b: Guild) => {
 		if (a.owner && !b.owner) return -1;
 		if (b.owner && !a.owner) return 1;
 		return a.name.localeCompare(b.name);
 	});
 
-	return guilds;
+	return guilds as Guild[];
 }
 
-export function (guild: Guild, permission: number) {
-	return guild.permissions & permission || guild.owner;
+export function CanEditGuild(guild: Guild) {
+	if (guild.owner) return true;
+
+	const perms = BigInt(guild.permissions);
+
+	const manageGuild = BigInt(0x20);
+	const manageEvents = BigInt(0x200000000);
+
+	return (perms & manageGuild) === manageGuild || (perms & manageEvents) === manageEvents;
 }
 
 export interface Guild {
@@ -38,7 +45,6 @@ export interface Guild {
     name: string,
     icon: string,
     owner: boolean,
-    permissions: number,
+    permissions: string,
     features: string[],
-    permissions_new: string,
 }
