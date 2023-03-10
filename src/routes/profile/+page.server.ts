b82c5bd8bb020835a3fb179ca29b9b4927568de8
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-03-10 11:32:37 -0500

    Use SvelteKit form actions

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,6 +1,8 @@
-import { FetchGuilds, type Guild } from '$lib/discord';
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import { GetUserByIGN, LinkDiscordUser, UnlinkDiscordUser } from '$db/database';
+import { FetchGuilds } from '$lib/discord';
+import { IsIGN } from '$params/ign';
+import { error, fail, redirect } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from './$types';
 
 export const load: PageServerLoad = async ({ parent, cookies }) => {
 	const { discordUser } = await parent();
@@ -15,7 +17,7 @@ export const load: PageServerLoad = async ({ parent, cookies }) => {
 		throw redirect(302, '/login');
 	}
 
-	const guilds = await FetchGuilds(token) as undefined | Guild[];
+	const guilds = await FetchGuilds(token);
 
 	if (!guilds) {
 		throw redirect(302, '/login');
@@ -25,3 +27,60 @@ export const load: PageServerLoad = async ({ parent, cookies }) => {
 		guilds
 	}
 };
+
+export const actions: Actions = {
+	link: async ({ locals, request }) => {
+		if (!locals.discordUser) {
+			throw error(401, 'Unauthorized');
+		}
+
+		const data = await request.formData();
+		const username = data.get('username')?.toString();
+
+		if (!username || !IsIGN(username)) {
+			return fail(400, { error: 'Invalid username.' });
+		}
+	
+		const foundUser = await GetUserByIGN(username);
+	
+		if (!foundUser) {
+			return fail(404, { error: 'User not found.' });
+		}
+	
+		const linkedName = foundUser.player?.player.socialMedia?.links?.DISCORD;
+	
+		if (!linkedName) {
+			return fail(400, { error: 'User has no linked account on Hypixel.' });
+		}
+	
+		const discordUser = `${locals.discordUser.username}#${locals.discordUser.discriminator}`;
+	
+		if (discordUser !== linkedName) {
+			return fail(400, { error: 'User has a different linked account on Hypixel.' });
+		}
+	
+		// Success!
+		await LinkDiscordUser(foundUser.uuid, locals.discordUser);
+
+		return { success: true };
+	},
+	unlink: async ({ locals }) => {
+		if (!locals.discordUser) {
+			throw error(401, 'Unauthorized');
+		}
+
+		if (!locals.user) {
+			return fail(400, { error: 'User not linked.' });
+		}
+
+		const uuid = locals.user.uuid;
+
+		if (!uuid) {
+			return fail(400, { error: 'Invalid UUID.' });
+		}
+
+		await UnlinkDiscordUser(uuid);
+
+		return { success: true };
+	}
+}
