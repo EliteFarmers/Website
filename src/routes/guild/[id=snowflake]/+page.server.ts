7f7c677219dffdc559b4dfdae7128e24a9ead75c
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-03-11 00:05:22 -0500

    Starting work on event creation

--- src/routes/guild/[id=snowflake]/+page.server.ts
+++ src/routes/guild/[id=snowflake]/+page.server.ts
@@ -1,6 +1,7 @@
-import { FetchGuilds } from '$lib/discord';
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import { GetOrCreateGuild } from '$db/events';
+import { CanEditFetchedGuild, FetchGuilds, GuildContainsBot } from '$lib/discord';
+import { error, fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ parent, cookies }) => {
 	const { discordUser } = await parent();
@@ -25,3 +26,38 @@ export const load: PageServerLoad = async ({ parent, cookies }) => {
 		guilds
 	}
 }
+
+export const actions: Actions = {
+	create: async ({ locals, request, params }) => {
+		const guildId = params.id;
+
+		if (!locals.discordUser || !guildId || !locals.discord_access_token) {
+			throw error(401, 'Unauthorized');
+		}
+
+		const hasPerms = await CanEditFetchedGuild(locals.discord_access_token, guildId);
+
+		if (!hasPerms) {
+			return fail(403, { error: 'You do not have permission to edit this guild.' });
+		}
+
+		const containsBot = await GuildContainsBot(guildId);
+
+		if (!containsBot) {
+			return fail(400, { error: 'This guild does not contain the bot.' });
+		}
+
+		const guild = await GetOrCreateGuild(guildId);
+
+		if (!guild) {
+			return fail(500, { error: 'An error occurred while creating the guild.' });
+		}
+
+		const data = await request.formData();
+		const name = data.get('name')?.toString();
+
+		return {
+			success: true,
+		};
+	}
+};
