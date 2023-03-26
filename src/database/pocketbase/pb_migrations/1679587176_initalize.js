787d02a2e69d053624cbf7217405a299e2a58ac6
Ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-03-26 13:36:21 -0400

    Add login info to .env

--- src/database/pocketbase/pb_migrations/1679587176_initalize.js
+++ src/database/pocketbase/pb_migrations/1679587176_initalize.js
@@ -2,15 +2,15 @@ migrate((db) => {
   // up
   const admin = new Admin()
 
-  admin.email = "test@example.com"
-  admin.setPassword("1234567890");
+  admin.email = process.env.PB_ADMIN_EMAIL
+  admin.setPassword(process.env.PB_ADMIN_PASSWORD);
 
   return Dao(db).saveAdmin(admin)
 }, (db) => {
   // down
   const dao = new Dao(db)
 
-  const admin = dao.findAdminByEmail("test@example.com")
+const admin = dao.findAdminByEmail(process.env.PB_ADMIN_EMAIL)
 
   return dao.deleteAdmin(admin)
 })
