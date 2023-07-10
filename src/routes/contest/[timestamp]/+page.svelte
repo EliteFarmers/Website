58df324b7055f640e7a0612704b9652badf0ddaa
ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-07-10 16:48:33 -0400

    Further work on contests

--- src/routes/contest/[timestamp]/+page.svelte
+++ src/routes/contest/[timestamp]/+page.svelte
@@ -1,42 +1,55 @@
 <script lang="ts">
-	import Head from "$comp/head.svelte";
 	import { getReadableSkyblockDate } from "$lib/format";
 	import type { PageData } from "./$types";
+
+	import Participation from "$comp/stats/contests/participation.svelte";
+	import Head from "$comp/head.svelte";
+	import { page } from "$app/stores";
 
 	export let data: PageData;
 
 	$: contests = data.contests;
 
+	$: timestamp = +$page.params.timestamp;
+
 	console.log(data.contests);
 </script>
 
 <Head 
-	title="Contests | {getReadableSkyblockDate(contests[0].timestamp ?? 0)}" 
+	title="Contests | {getReadableSkyblockDate(timestamp ?? 0)}" 
 	description="View all known participations of these Jacob contests!"
 ></Head>
 
 <div class="flex flex-col items-center justify-center">
-	<h1 class="text-3xl font-bold text-center text-gray-900 dark:text-zinc-400">
-		{getReadableSkyblockDate(contests[0].timestamp ?? 0)}
-	</h1>
-	<div class="grid grid-cols-1 gap-4 mt-4 lg:grid-cols-3">
+	<div class="font-semibold text-center mt-16 mb-4 flex flex-col gap-4">
+		<h1 class="text-4xl">{getReadableSkyblockDate(timestamp ?? 0)}</h1>
+
+		<div class="flex flex-row justify-center gap-8 my-4">
+			<a class="flex-1 bg-gray-200 dark:bg-zinc-800 p-2 rounded-lg" href="/contest/{timestamp - 3600}">Previous</a>
+			<a class="flex-1 bg-gray-200 dark:bg-zinc-800 p-2 rounded-lg" href="/contest/{timestamp + 3600}">Next</a>
+		</div>
+	</div>
+	
+	{#if contests.length === 0}
+		<div class="flex flex-col items-center justify-center p-4 space-y-2">
+			<h2 class="text-3xl font-semibold text-center">
+				No Contests Found
+			</h2>
+			<h4>Try a different timestamp!</h4>
+			<p>Data will be loaded once a player has participated in once of these contests!</p>
+		</div>
+	{/if}
+
+	<div class="grid grid-cols-1 gap-4 mt-4 lg:grid-cols-3 mb-16">
 		{#each contests as contest}
 			<div class="flex flex-col items-center justify-start p-4 space-y-2 bg-white rounded-md shadow-md dark:bg-zinc-800">
-				<h2 class="text-xl font-bold text-center text-gray-900 dark:text-zinc-400">
+				<h2 class="text-3xl font-semibold text-center">
 					{contest.crop ?? 'Not Found'}
 				</h2>
+				<h4>{contest.participations?.length ?? 0} / {contest.participants} Participants</h4>
 				<div class="flex flex-col justify-center space-y-2">
 					{#each contest.participations?.sort((a, b) => (b?.collected ?? 0) - (a?.collected ?? 0)) ?? [] as participant}
-						<a
-							href={`/stats/${participant.playerName}/`}
-							class="text-lg font-bold text-center hover:underline"
-						>
-							<div class="flex flex-row justify-start items-start gap-4">
-								<span>#{participant.position !== -1 ? (participant.position ?? 0) + 1 : '???'}</span>
-								<span>{participant.playerName}</span>
-								<span class="w-full text-right">{participant.collected?.toLocaleString()}</span>
-							</div>
-						</a>
+						<Participation entry={participant} />
 					{/each}
 				</div>
 			</div>
