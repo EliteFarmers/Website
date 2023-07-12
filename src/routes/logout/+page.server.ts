e23e42fb91b9811805529ec43bc04a0ba38ef6ac
ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-07-12 01:15:17 -0400

    Remove unused endpoints

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -3,7 +3,6 @@ import type { ServerLoad } from '@sveltejs/kit';
 export const load: ServerLoad = ({ cookies }) => {
 	cookies.delete('discord_access_token');
 	cookies.delete('discord_refresh_token');
-	cookies.delete('pocketbase_auth');
 
 	return {
 		status: 200,
