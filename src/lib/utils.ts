3f31917a6d60b69ad029e259fb8e9f3c9dc7cbbd
ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-07-25 18:09:53 -0400

    Discord channel types

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,18 @@ export function CanManageGuild(permissions?: string) {
 	// Check if the user has the manage guild or manage events permission
 	return (perms & admin) === admin || (perms & manageGuild) === manageGuild;
 }
+
+export enum ChannelType {
+    GuildText = 0,
+    DirectMessage = 1,
+    GuildVoice = 2,
+    GroupDirectMessage = 3,
+    GuildCategory = 4,
+    GuildAnnouncement = 5,
+    AnnouncementThread = 10,
+    PublicThread = 11,
+    PrivateThread = 12,
+    GuildStage = 13,
+    GuildDirectory = 14,
+    GuildForum = 15
+}
