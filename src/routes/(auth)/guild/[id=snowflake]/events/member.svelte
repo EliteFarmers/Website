c24d0c31866bf9f1333f79ee8a70e2ca236164bf
ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-08-20 16:27:13 -0400

    Seperate out listed members and improve formatting

---
+++ src/routes/(auth)/guild/[id=snowflake]/events/member.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { components } from '$lib/api/api';
+
+	export let member: components['schemas']['EventMemberDto'];
+</script>
+
+<div class="flex items-center justify-between flex-row gap-4 max-w-2xl w-full space-y-2 text-black dark:text-white">
+	<div class="flex flex-row gap-4">
+		<slot />
+		<img
+			src="https://mc-heads.net/avatar/{member.playerUuid}"
+			class="w-8 aspect-square rounded-sm"
+			alt="Player Head"
+		/>
+		<p class="text-lg">{member.playerName}</p>
+	</div>
+	<div class="flex flex-row gap-4">
+		{#if member.notes || member.status === 2}
+			<p>{member.notes || (member.status === 2 ? 'Member Left' : '')}</p>
+		{/if}
+		<p class="font-semibold text-xl">{(+(member.amountGained ?? 0)).toLocaleString()}</p>
+	</div>
+</div>
