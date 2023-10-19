748a369c749cbb5f1bee3dc7f4f7b03e59ad32cb
ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-10-19 18:47:29 -0400

    Display Elite Farmer's first on /browse

--- src/routes/browse/+page.svelte
+++ src/routes/browse/+page.svelte
@@ -2,11 +2,17 @@
 	import type { PageData } from './$types';
 	import Serverbar from '$comp/stats/discord/serverbar.svelte';
 	import Event from '$comp/stats/discord/event.svelte';
+	import Head from '$comp/head.svelte';
+	import { PUBLIC_COMMUNITY_ID } from '$env/static/public';
 
 	export let data: PageData;
 	$: events = data.events ?? [];
-	$: guilds = data.guilds ?? [];
+
+	$: pinned = (data.guilds ?? []).find((g) => g.id === PUBLIC_COMMUNITY_ID);
+	$: guilds = (data.guilds ?? []).filter((g) => g.id !== PUBLIC_COMMUNITY_ID);
 </script>
+
+<Head title="Browse Servers" description="Browse Discord servers and Events available to join!" />
 
 <main class="flex flex-col justify-center items-center text-center my-16" data-sveltekit-preload-data="tap">
 	{#if events.length > 0}
@@ -21,7 +27,10 @@
 	<section class="flex flex-col gap-8 w-[90%] md:w-[70%] max-w-7xl my-16 text-white">
 		<h1 class="text-4xl">Explore Public Discord Servers</h1>
 		<p class="text-xl my-4">Look through the Discord servers with Jacob Leaderboard access!</p>
-		{#each data.guilds ?? [] as guild (guild.id)}
+		{#if pinned}
+			<Serverbar guild={pinned} />
+		{/if}
+		{#each guilds as guild (guild.id)}
 			<Serverbar {guild} />
 		{/each}
 	</section>
