eb6822746b2fd4ad9d25acf7a67899a1f5cbe56c
ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-10-31 01:45:03 -0400

    Seperate components out

---
+++ src/lib/stores/ratesData.ts
@@ -0,0 +1,34 @@
+import { browser } from "$app/environment";
+import type { Crop } from "farming-weight";
+import { writable } from "svelte/store";
+
+interface RatesData { 
+	milestones: Partial<Record<Crop, number>>;
+	cropUpgrades: Partial<Record<Crop, number>>;
+	gardenLevel: number;
+	plotsUnlocked: number;
+}
+
+// Initialize the store with the data from localStorage if it exists
+let defaultData = {
+	milestones: {},
+	cropUpgrades: {},
+	gardenLevel: 0,
+	plotsUnlocked: 0,
+} satisfies RatesData;
+
+if (browser) {
+	const savedRatesData = localStorage.getItem('ratesData');
+
+	if (savedRatesData) {
+		defaultData = JSON.parse(savedRatesData) as RatesData;
+	}
+}
+
+export const ratesData = writable<RatesData>(defaultData);
+
+ratesData.subscribe((rates) => {
+	if (browser) {
+		localStorage.setItem('ratesData', JSON.stringify(rates));
+	}
+});
