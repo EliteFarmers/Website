018dd8c56c78ab7510b2c291cf1f8083a922d425
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-01-04 23:47:02 -0600

    Add discord icon components

---
+++ src/components/stats/discord/guildicon.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { components } from '$lib/api/api';
+	export let guild: components['schemas']['GuildDetailsDto'] | undefined;
+	export let size = 12;
+</script>
+
+{#if !guild?.icon}
+	<div class="w-{size} h-{size} rounded-full flex items-center justify-center select-none bg-blend-darken bg-black">
+		<p>
+			{guild?.name
+				?.split(' ')
+				.slice(0, 3)
+				.map((word) => word[0])
+				.join('')}
+		</p>
+	</div>
+{:else}
+	<img
+		loading="lazy"
+		class="w-{size} h-{size} rounded-full"
+		src={`https://cdn.discordapp.com/icons/${guild?.id}/${guild?.icon}.${
+			guild?.icon.startsWith('a_') ? 'gif' : 'webp'
+		}?size=96`}
+		alt="Server Icon"
+	/>
+{/if}
---
+++ src/components/stats/discord/usericon.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { AuthorizedUser } from '$lib/api/elite';
+	import { UserOutline } from 'flowbite-svelte-icons';
+	export let user: AuthorizedUser;
+	export let size = 12;
+</script>
+
+{#if !user.avatar}
+	<div class="w-{size} h-{size} rounded-full flex items-center justify-center select-none bg-blend-darken bg-black">
+		<UserOutline />
+	</div>
+{:else}
+	<img
+		loading="lazy"
+		class="w-{size} h-{size} rounded-full"
+		src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.${
+			user.avatar.startsWith('a_') ? 'gif' : 'webp'
+		}?size=96`}
+		alt="Server Icon"
+	/>
+{/if}
