b0530edcf90e7dc8d0a69164dc5592c5950f47f2
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-01-23 23:59:18 -0500

    Remake main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,8 @@
 
 	import Head from '$comp/head.svelte';
 	import Entry from '$comp/leaderboards/entry.svelte';
-	import { Card, Button } from 'flowbite-svelte';
+	import * as Card from '$ui/card';
+	import { Button } from '$ui/button';
 
 	import type { LeaderboardEntry } from '$lib/api/elite';
 	import type { PageData } from './$types';
@@ -22,7 +23,7 @@
 
 <main>
 	<h1 class="text-4xl text-center my-16">Welcome to Elite!</h1>
-	<p class="text-body-xl text-center mb-16">View farming stats of any skyblock player!</p>
+	<p class="text-lg text-center mb-16">View farming stats of any skyblock player!</p>
 
 	<div class="flex flex-col w-full items-center gap-8">
 		{#if data.eliteGuild}
@@ -32,94 +33,111 @@
 		{/if}
 		<div class="flex flex-col md:flex-row gap-4 justify-center items-center">
 			<div class="flex flex-col items-center gap-4">
-				<Card color="none" border={false} class="dark:bg-zinc-800">
-					<h1 class="mb-4 w-full text-xl font-semibold">Join The Discord</h1>
-					<p class="w-full mb-6">
-						Full membership unlocked after reaching {(+PUBLIC_WEIGHT_REQ).toLocaleString()}
-						farming weight. Also, join the support server for suggestions and bug reports!
-					</p>
-					<div class="flex flex-col md:flex-row gap-2 justify-center">
-						<Button
-							href="/discord"
-							class="w-fit font-semibold"
-							color="blue"
-							target="_blank"
-							rel="noopener noreferrer nofollow"
-						>
-							Elite Farmers
-							<ArrowUpRightFromSquareOutline class="ml-2" size="sm" />
-						</Button>
-						<Button
-							href="/support"
-							class="w-fit font-semibold"
-							color="blue"
-							target="_blank"
-							rel="noopener noreferrer nofollow"
-						>
-							Support Server
-							<ArrowUpRightFromSquareOutline class="ml-2" size="sm" />
-						</Button>
-					</div>
-				</Card>
-				<Card color="none" border={false} class="dark:bg-zinc-800">
-					<h1 class="mb-4 w-full text-xl font-semibold">Support Development!</h1>
-					<p class="w-full mb-6">
-						Donate on Ko-Fi to support the development of Elite! Also check Server Subscriptions on the
-						development server to unlock some perks!
-					</p>
-					<div class="flex justify-center">
-						<Button
-							href={PUBLIC_DONATION_URL}
-							class="w-fit font-semibold"
-							target="_blank"
-							color="green"
-							rel="noopener noreferrer nofollow"
-						>
-							Donate on Ko-Fi
-							<ArrowUpRightFromSquareOutline class="ml-2" size="sm" />
-						</Button>
-					</div>
-				</Card>
+				<Card.Root class="max-w-md">
+					<Card.Header>
+						<Card.Title class="text-xl">Join The Discord</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<p class="w-full mb-6 text-lg">
+							Full membership unlocked after reaching {(+PUBLIC_WEIGHT_REQ).toLocaleString()}
+							farming weight. Also, join the support server for suggestions and bug reports!
+						</p>
+						<div class="flex flex-col md:flex-row gap-2 justify-center">
+							<Button
+								href="/discord"
+								class="w-fit font-semibold"
+								variant="secondary"
+								target="_blank"
+								rel="noopener noreferrer nofollow"
+							>
+								Elite Farmers
+								<ArrowUpRightFromSquareOutline class="ml-2" size="sm" />
+							</Button>
+							<Button
+								href="/support"
+								class="w-fit font-semibold"
+								variant="secondary"
+								target="_blank"
+								rel="noopener noreferrer nofollow"
+							>
+								Support Server
+								<ArrowUpRightFromSquareOutline class="ml-2" size="sm" />
+							</Button>
+						</div>
+					</Card.Content>
+				</Card.Root>
+				<Card.Root class="max-w-md">
+					<Card.Header>
+						<Card.Title class="text-xl">Support Development!</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<p class="w-full mb-6 text-lg">
+							Donate on Ko-Fi to support the development of Elite! Also check Server Subscriptions on the
+							development server to unlock some perks!
+						</p>
+						<div class="flex justify-center">
+							<Button
+								href={PUBLIC_DONATION_URL}
+								class="w-fit font-semibold"
+								target="_blank"
+								variant="secondary"
+								rel="noopener noreferrer nofollow"
+							>
+								Donate on Ko-Fi
+								<ArrowUpRightFromSquareOutline class="ml-2" size="sm" />
+							</Button>
+						</div>
+					</Card.Content>
+				</Card.Root>
 			</div>
 			<div class="flex flex-col items-center gap-4">
-				<Card color="none" border={false} class="dark:bg-zinc-800">
-					<h1 class="mb-4 w-full text-xl font-semibold">Add To Discord</h1>
-					<p class="w-full mb-6">
-						Quickly access stats and leaderboards in Discord! Try out commands like <strong>/weight</strong
-						>, <strong>/rates</strong>, and <strong>/leaderboard</strong>!
-					</p>
-					<div class="flex justify-center">
-						<Button
-							href="/invite"
-							class="w-fit font-semibold"
-							target="_blank"
-							color="blue"
-							rel="noopener noreferrer nofollow"
-						>
-							Invite Elite Bot
-							<ArrowUpRightFromSquareOutline class="ml-2" size="sm" />
-						</Button>
-					</div>
-				</Card>
-				<Card color="none" border={false} class="dark:bg-zinc-800">
-					<h1 class="mb-4 w-full text-xl font-semibold">Purchase Crop Stickers!</h1>
-					<p class="w-full mb-6">
-						Direcly support the art on Elite by purchasing crop stickers! All proceeds go to the artist, and
-						you get a cool sticker!
-					</p>
-					<div class="flex justify-center">
-						<Button
-							href="https://www.etsy.com/listing/1499421785/pixelated-crop-stickers"
-							class="w-fit font-semibold"
-							target="_blank"
-							color="green"
-							rel="noopener noreferrer nofollow"
-						>
-							Open Lumini's Shop
-							<ArrowUpRightFromSquareOutline class="ml-2" size="sm" />
-						</Button>
-					</div>
-				</Card>
+				<Card.Root class="max-w-md">
+					<Card.Header>
+						<Card.Title class="text-xl">Add To Discord</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<p class="w-full mb-6 text-lg">
+							Quickly access stats and leaderboards in Discord! Try out commands like <strong
+								>/weight</strong
+							>, <strong>/rates</strong>, and <strong>/leaderboard</strong>!
+						</p>
+						<div class="flex justify-center">
+							<Button
+								href="/invite"
+								class="w-fit font-semibold"
+								target="_blank"
+								variant="secondary"
+								rel="noopener noreferrer nofollow"
+							>
+								Invite Elite Bot
+								<ArrowUpRightFromSquareOutline class="ml-2" size="sm" />
+							</Button>
+						</div>
+					</Card.Content>
+				</Card.Root>
+				<Card.Root class="max-w-md">
+					<Card.Header>
+						<Card.Title class="text-xl">Purchase Crop Stickers!</Card.Title>
+					</Card.Header>
+					<Card.Content>
+						<p class="w-full mb-6 text-lg">
+							Direcly support the art on Elite by purchasing crop stickers! All proceeds go to the artist,
+							and you get a cool sticker!
+						</p>
+						<div class="flex justify-center">
+							<Button
+								href="https://www.etsy.com/listing/1499421785/pixelated-crop-stickers"
+								class="w-fit font-semibold"
+								target="_blank"
+								variant="secondary"
+								rel="noopener noreferrer nofollow"
+							>
+								Open Lumini's Shop
+								<ArrowUpRightFromSquareOutline class="ml-2" size="sm" />
+							</Button>
+						</div>
+					</Card.Content>
+				</Card.Root>
 			</div>
 		</div>
 	</div>
