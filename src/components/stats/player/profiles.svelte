7b7083e6211bd24236d715f656d996ec31f9ca99
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-03-16 20:51:39 -0400

    Fix overlapping popover

--- src/components/stats/player/profiles.svelte
+++ src/components/stats/player/profiles.svelte
@@ -8,36 +8,34 @@
 	export let profiles: ProfileDetails[];
 </script>
 
-<Popover.Mobile hasContent={profiles.length > 0}>
-	<div slot="trigger">
-		<div class="grid col-span-1 z-10">
-			<div class="flex flex-row gap-2 items-center">
-				<div class="p-1 lg:p-2 px-2 mx-1 bg-primary-foreground rounded-md">
-					<h2 class="text-2xl md:text-3xl">
-						{selected.name}
-					</h2>
-				</div>
-				<Gamemode
-					class="first-letter:capitalize font-semibold text-2xl text-muted-foreground"
-					gameMode={selected.gameMode}
-				/>
+<div class="flex flex-row gap-2 items-center">
+	<Popover.Mobile hasContent={profiles.length > 0}>
+		<div slot="trigger" class="grid col-span-1 z-10">
+			<div class="p-1 lg:p-2 px-2 mx-1 bg-primary-foreground rounded-md">
+				<h2 class="text-2xl md:text-3xl">
+					{selected.name}
+				</h2>
 			</div>
 		</div>
-	</div>
-	{#if profiles.length > 0}
-		<div class="flex flex-col gap-1" data-sveltekit-preload-data="tap">
-			{#each profiles ?? [] as pId (pId.id)}
-				<a
-					href={`/@${ign}/${pId.name}`}
-					class="p-2 text-xl font-semibold flex flex-row gap-4 justify-between hover:bg-muted rounded-sm"
-				>
-					<div class="flex flex-row gap-2 items-center">
-						<span>{pId.name}</span>
-						<Gamemode popover={false} gameMode={pId.gameMode} />
-					</div>
-					<span class="font-normal">{pId.weight.toLocaleString()}</span>
-				</a>
-			{/each}
-		</div>
-	{/if}
-</Popover.Mobile>
+		{#if profiles.length > 0}
+			<div class="flex flex-col gap-1" data-sveltekit-preload-data="tap">
+				{#each profiles ?? [] as pId (pId.id)}
+					<a
+						href={`/@${ign}/${pId.name}`}
+						class="p-2 text-xl font-semibold flex flex-row gap-4 justify-between hover:bg-muted rounded-sm"
+					>
+						<div class="flex flex-row gap-2 items-center">
+							<span>{pId.name}</span>
+							<Gamemode popover={false} gameMode={pId.gameMode} />
+						</div>
+						<span class="font-normal">{pId.weight.toLocaleString()}</span>
+					</a>
+				{/each}
+			</div>
+		{/if}
+	</Popover.Mobile>
+	<Gamemode
+		class="first-letter:capitalize font-semibold text-2xl text-muted-foreground"
+		gameMode={selected.gameMode}
+	/>
+</div>
