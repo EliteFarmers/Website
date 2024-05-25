0c3cd17ccf5d2936069d3d9d033eff79642f14bd
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-05-24 23:14:18 -0400

    Clarify when tool crop values are zero

--- src/components/items/tools/farmingtool.svelte
+++ src/components/items/tools/farmingtool.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import { getCropDisplayName, type FarmingTool as FT } from 'farming-weight';
+	import { FarmingToolType, getCropDisplayName, type FarmingTool as FT } from 'farming-weight';
 	import { FormatMinecraftText } from '$lib/format';
 	import Fortunebreakdown from './fortunebreakdown.svelte';
 	import { PROPER_CROP_TO_IMG } from '$lib/constants/crops';
 	import Lorebtn from '../lorebtn.svelte';
+	import { UNUSED_MATHEMATICAL_HOE } from '$content/random';
 
 	export let tool: FT;
 </script>
@@ -20,7 +21,15 @@
 			<div class="font-semibold text-md md:text-lg">{@html FormatMinecraftText(tool.item.name ?? '')}</div>
 			<div class="flex flex-row gap-1 items-center">
 				<img src={PROPER_CROP_TO_IMG[getCropDisplayName(tool.crop)]} alt="Crop" class="pixelated w-5 h-5" />
-				<div class="text-md md:text-lg">{tool.farmed.toLocaleString()}</div>
+				{#if tool.farmed > 0}
+					<div class="text-md md:text-lg">{tool.farmed.toLocaleString()}</div>
+				{:else if tool.tool.type === FarmingToolType.MathematicalHoe}
+					<div class="text-sm md:text-md text-muted-foreground">
+						{UNUSED_MATHEMATICAL_HOE[Math.floor(Math.random() * UNUSED_MATHEMATICAL_HOE.length)]}
+					</div>
+				{:else}
+					<div class="text-sm md:text-md text-muted-foreground">Missing Cultivating!</div>
+				{/if}
 			</div>
 		</div>
 	</div>
