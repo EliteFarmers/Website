5372d82f8fbab4b3eba31a819e7f20e44f73107b
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-06-08 14:10:50 -0400

    Fix circular import

--- src/constants/upgrades.ts
+++ src/constants/upgrades.ts
@@ -1,4 +1,4 @@
-import { GearSlot } from "./armor";
+import type { GearSlot } from "./armor";
 import { Crop } from "./crops";
 import { FARMING_TOOLS } from "./tools";
 
@@ -20,28 +20,28 @@ export interface InitialItems {
 	pets: string[];
 }
 
-export const INITIAL_ITEMS: InitialItems = {
-	tools: {
-		[Crop.Cactus]: 'CACTUS_KNIFE',
-		[Crop.CocoaBeans]: 'COCO_CHOPPER',
-		[Crop.Carrot]: 'THEORETICAL_HOE_CARROT_1',
-		[Crop.Melon]: 'MELON_DICER',
-		[Crop.Mushroom]: 'FUNGI_CUTTER',
-		[Crop.NetherWart]: 'THEORETICAL_HOE_WARTS_1',
-		[Crop.Potato]: 'THEORETICAL_HOE_POTATO_1',
-		[Crop.Pumpkin]: 'PUMPKIN_DICER',
-		[Crop.SugarCane]: 'THEORETICAL_HOE_CANE_1',
-		[Crop.Wheat]: 'THEORETICAL_HOE_WHEAT_1'
-	},
-	armor: {
-		[GearSlot.Boots]: [ 'MELON_BOOTS', 'FARMER_BOOTS', 'RANCHERS_BOOTS' ],
-		[GearSlot.Leggings]: 'MELON_LEGGINGS',
-		[GearSlot.Chestplate]: 'MELON_CHESTPLATE',
-		[GearSlot.Helmet]: 'MELON_HELMET',
-		[GearSlot.Necklace]: 'LOTUS_NECKLACE',
-		[GearSlot.Cloak]: 'LOTUS_CLOAK',
-		[GearSlot.Belt]: 'LOTUS_BELT',
-		[GearSlot.Gloves]: 'LOTUS_BRACELET',
-	},
-	pets: [ 'ELEPHANT', 'MOOSHROOM_COW' ]
-};
+// export const INITIAL_ITEMS: InitialItems = {
+// 	tools: {
+// 		[Crop.Cactus]: 'CACTUS_KNIFE',
+// 		[Crop.CocoaBeans]: 'COCO_CHOPPER',
+// 		[Crop.Carrot]: 'THEORETICAL_HOE_CARROT_1',
+// 		[Crop.Melon]: 'MELON_DICER',
+// 		[Crop.Mushroom]: 'FUNGI_CUTTER',
+// 		[Crop.NetherWart]: 'THEORETICAL_HOE_WARTS_1',
+// 		[Crop.Potato]: 'THEORETICAL_HOE_POTATO_1',
+// 		[Crop.Pumpkin]: 'PUMPKIN_DICER',
+// 		[Crop.SugarCane]: 'THEORETICAL_HOE_CANE_1',
+// 		[Crop.Wheat]: 'THEORETICAL_HOE_WHEAT_1'
+// 	},
+// 	armor: {
+// 		[GearSlot.Boots]: [ 'MELON_BOOTS', 'FARMER_BOOTS', 'RANCHERS_BOOTS' ],
+// 		[GearSlot.Leggings]: 'MELON_LEGGINGS',
+// 		[GearSlot.Chestplate]: 'MELON_CHESTPLATE',
+// 		[GearSlot.Helmet]: 'MELON_HELMET',
+// 		[GearSlot.Necklace]: 'LOTUS_NECKLACE',
+// 		[GearSlot.Cloak]: 'LOTUS_CLOAK',
+// 		[GearSlot.Belt]: 'LOTUS_BELT',
+// 		[GearSlot.Gloves]: 'LOTUS_BRACELET',
+// 	},
+// 	pets: [ 'ELEPHANT', 'MOOSHROOM_COW' ]
+// };
