632db67a47ece6b02e5c95c5519fa5a277faedca
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-07-20 14:48:53 -0400

    Add reload button to error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -10,10 +10,15 @@
 
 <h1 class="text-center font-bold text-6xl p-16 m-16 font-mono">{status}</h1>
 <h4 class="text-center font-lighter text-2xl">{message}</h4>
-<div class="text-center m-12 mb-32">
+<div class="flex flex-row justify-center gap-4 item text-center m-12 mb-32">
 	<a
 		href="/"
 		class="p-4 shadow-md rounded-md shadow-gray bg-gray-300 dark:bg-zinc-800 hover:bg-gray-200 dark:hover:bg-zinc-700"
 		>Return Home</a
+	>
+	<button
+		on:click={() => location.reload()}
+		class="p-4 shadow-md rounded-md shadow-gray bg-gray-300 dark:bg-zinc-800 hover:bg-gray-200 dark:hover:bg-zinc-700"
+		>Reload</button
 	>
 </div>
