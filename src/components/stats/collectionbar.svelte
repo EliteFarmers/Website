aa614a2b0bb2a4af1e2e68e34a9cbddefe8079d2
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-07-29 19:25:58 -0400

    Improve collection bar layouts

--- src/components/stats/collectionbar.svelte
+++ src/components/stats/collectionbar.svelte
@@ -41,22 +41,22 @@
 	}
 </script>
 
-<div class="flex flex-row justify-between gap-2 w-full align-middle items-center">
-	<div class="flex flex-1 justify-between align-middle w-full max-h-30 bg-primary-foreground rounded-lg p-1">
-		<div class="flex flex-row justify-start items-center gap-2 w-full">
-			{#key rank}
-				<div
-					class="flex crop-container pixelated w-10 h-10 sm:w-14 sm:h-14 md:w-20 md:h-20 aspect-square"
-					style={getFrameStyle()}
-				>
-					<img
-						src="/images/crops/{key}.png"
-						class="rounded-lg pixelated aspect-square p-[16%] object-contain"
-						alt={name}
-					/>
-				</div>
-			{/key}
-			<div class="flex flex-col align-middle justify-center w-full">
+<div class="flex flex-row gap-2 w-full align-middle items-center">
+	<div class="flex flex-1 gap-2 justify-start align-middle w-full max-h-30 bg-primary-foreground rounded-lg p-1">
+		{#key rank}
+			<div
+				class="flex crop-container pixelated w-10 h-10 sm:w-14 sm:h-14 md:w-20 md:h-20 aspect-square"
+				style={getFrameStyle()}
+			>
+				<img
+					src="/images/crops/{key}.png"
+					class="rounded-lg pixelated aspect-square p-[16%] object-contain"
+					alt={name}
+				/>
+			</div>
+		{/key}
+		<div class="flex flex-col gap-1 justify-center flex-grow pr-2">
+			<div class="flex flex-row justify-between items-center gap-2">
 				<div class="flex flex-row items-center gap-1">
 					{#if rank > 0}
 						<a
@@ -70,47 +70,48 @@
 					{/if}
 					<p class="text-md sm:text-lg font-semibold whitespace-nowrap">{name}</p>
 				</div>
-				<p class="text-normal sm:text-lg md:text-xl lg:text-2xl whitespace-nowrap">{value.toLocaleString()}</p>
+				<p class="md:ml-2 text-right font-semibold sm:text-lg md:text-xl lg:text-2xl">
+					{weight.toLocaleString()}
+				</p>
 			</div>
-		</div>
-
-		<div class="flex flex-col justify-center align-middle w-full p-1 gap-1">
-			<p class="md:ml-2 text-right font-semibold sm:text-lg md:text-xl lg:text-2xl">{weight.toLocaleString()}</p>
-			<div class="md:ml-2 text-right flex flex-row items-center gap-2 justify-end">
-				{#if pestRank > 0}
-					<a
-						href="/leaderboard/{pest}/{$page.params.id}-{$page.params.profile}"
-						class="px-1 bg-card rounded-md hover:bg-muted"
-					>
-						<span class="text-sm xs:text-md sm:text-lg">#</span><span class="text-md xs:text-lg sm:text-xl"
-							>{pestRank}</span
+			<div class="flex flex-row justify-between items-center gap-2">
+				<p class="text-normal sm:text-lg md:text-xl lg:text-2xl whitespace-nowrap">{value.toLocaleString()}</p>
+				<div class="flex flex-row items-center gap-2">
+					{#if pestRank > 0}
+						<a
+							href="/leaderboard/{pest}/{$page.params.id}-{$page.params.profile}"
+							class="px-1 bg-card rounded-md hover:bg-muted"
 						>
-					</a>
-				{/if}
-				<Popover.Mobile>
-					<div slot="trigger" class="flex flex-row items-center align-middle justify-center gap-2 h-6">
-						<p class="text-md sm:text-lg font-semibold whitespace-nowrap">
-							{pestKills.toLocaleString()}
-						</p>
-						<img
-							src="/images/pests/{pest}.png"
-							class="pixelated aspect-square object-contain h-full"
-							alt={pest}
-						/>
-					</div>
-					<div class="flex flex-col items-center gap-2 max-w-md">
-						<p class="text-lg first-letter:capitalize font-semibold">{pest} Kills</p>
-						<p>{pestKills.toLocaleString()}</p>
-						<a class="text-lg font-semibold text-blue-500 hover:underline" href="/info#Pests"
-							>Weight Adjustment</a
-						>
-						{#if uncounted === 0}
-							<p>None!</p>
-						{:else}
-							<p>-{uncounted.toLocaleString()} {name}</p>
-						{/if}
-					</div>
-				</Popover.Mobile>
+							<span class="text-sm xs:text-md sm:text-lg">#</span><span
+								class="text-md xs:text-lg sm:text-xl">{pestRank}</span
+							>
+						</a>
+					{/if}
+					<Popover.Mobile>
+						<div slot="trigger" class="flex flex-row items-center align-middle justify-center gap-2 h-6">
+							<p class="text-md sm:text-lg font-semibold whitespace-nowrap">
+								{pestKills.toLocaleString()}
+							</p>
+							<img
+								src="/images/pests/{pest}.png"
+								class="pixelated aspect-square object-contain h-full"
+								alt={pest}
+							/>
+						</div>
+						<div class="flex flex-col items-center gap-2 max-w-md">
+							<p class="text-lg first-letter:capitalize font-semibold">{pest} Kills</p>
+							<p>{pestKills.toLocaleString()}</p>
+							<a class="text-lg font-semibold text-blue-500 hover:underline" href="/info#Pests"
+								>Weight Adjustment</a
+							>
+							{#if uncounted === 0}
+								<p>None!</p>
+							{:else}
+								<p>-{uncounted.toLocaleString()} {name}</p>
+							{/if}
+						</div>
+					</Popover.Mobile>
+				</div>
 			</div>
 		</div>
 	</div>
