b1f9b3a18c017951f9b5a3675966feef6972cf61
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-08-01 19:40:52 -0400

    Lazy load server bar splash

--- src/components/stats/discord/serverbar.svelte
+++ src/components/stats/discord/serverbar.svelte
@@ -8,14 +8,17 @@
 
 <a
 	href="/server/{guild.id}"
-	class="relative flex flex-row justify-between flex-1 p-8 py-8 bg-cover bg-no-repeat bg-center rounded-lg w-full bg-primary-foreground {guild.banner
+	class="relative flex flex-row justify-between flex-1 p-8 py-8 rounded-lg w-full items-center bg-primary-foreground overflow-clip {guild.banner
 		? 'text-white'
 		: ''}"
-	style={guild.banner
-		? `background-image: url('https://cdn.discordapp.com/splashes/${guild.id}/${guild?.banner}.png?size=1280')`
-		: ''}
 >
 	{#if guild.banner}
+		<img
+			class="absolute left-0 right-0 rounded-lg w-full pixelated"
+			loading="lazy"
+			src="https://cdn.discordapp.com/splashes/{guild.id}/{guild?.banner}.png?size=640"
+			alt="Server Banner"
+		/>
 		<div
 			class="absolute top-0 left-0 right-0 bottom-0 rounded-lg bg-gradient-to-r from-zinc-900/70 via-transparent to-zinc-900/70"
 		/>
