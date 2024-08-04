30b3ed02e0d6782241486130315f9b381fb80e75
Kaeso <24925519+ptlthg@users.noreply.github.com>
2024-08-04 00:17:21 -0400

    Fix Event Embed (#263)

--- src/routes/event/[event=slug]/+page.svelte
+++ src/routes/event/[event=slug]/+page.svelte
@@ -49,11 +49,11 @@
 	$: topList = teamEvent
 		? teams
 				.slice(0, 5)
-				.map((team, i) => `${i + 1}. ${team.name} • ${+(team?.score ?? 0).toLocaleString()}`)
+				.map((team, i) => `${i + 1}. ${team.name} • ${(+(team?.score ?? 0)).toLocaleString()}`)
 				.join('\n')
 		: members
 				.slice(0, 5)
-				.map((member, i) => `${i + 1}. ${member.playerName} • ${+(member?.score ?? 0).toLocaleString()}`)
+				.map((member, i) => `${i + 1}. ${member.playerName} • ${(+(member?.score ?? 0)).toLocaleString()}`)
 				.join('\n');
 
 	$: description = `View the ${running ? 'Event happening' : 'past Event'} in ${data.guild?.name}!\n\n${topList}`;
