bd53a49a1a1cdc4c8e8b436a6c05b81d5cb958ea
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-10-01 21:14:53 -0400

    Fix fortune progress issues

--- src/components/jump-link.svelte
+++ src/components/jump-link.svelte
@@ -5,13 +5,16 @@
 	export let id: string;
 	export let size = 20;
 	export let self = true;
+
+	let className = '';
+	export { className as class };
 </script>
 
 <Button
 	href="#{id}"
 	variant="ghost"
 	size="sm"
-	class="mx-2 scroll-mt-32 text-gray-500 hover:text-inherit"
+	class="mx-2 scroll-mt-32 text-gray-500 hover:text-inherit {className}"
 	id={self ? id : undefined}
 >
 	<Link {size} />
