32aaff0660ce7b7cdafff283f07bf0123c790b65
Kaeso <24925519+ptlthg@users.noreply.github.com>
2024-10-20 16:15:44 -0400

    Add contact page (#317)
    * Move dependencies to devDependencies for better builds
    
    * Update API terms and add contact page

--- src/components/external-link-button.svelte
+++ src/components/external-link-button.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
 	import ExternalLink from 'lucide-svelte/icons/external-link';
-	import { Button } from '$ui/button';
 
 	export let href: string;
+	export let text: string | undefined = undefined;
+	export let icon = true;
 </script>
 
-<Button variant="link" {href} class="decoration-blue-500">
-	<div class="flex flex-row items-center gap-1 text-blue-500">
-		<span>
-			<slot />
-		</span>
-		<ExternalLink size={16} />
-	</div>
-</Button>
+<a {href} class="inline-block text-blue-500 decoration-blue-500">
+	<span class="flex flex-row items-center gap-1">
+		{#if text}
+			<span>{text}</span>
+		{:else}
+			<span>
+				<slot />
+			</span>
+		{/if}
+		{#if icon}
+			<ExternalLink size={16} class="mt-0.5" />
+		{/if}
+	</span>
+</a>
