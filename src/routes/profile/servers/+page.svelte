1c189dfd2c70420c31146f3d0fa8ca95b337e530
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-11-22 16:37:31 -0500

    Display "Manage Servers" first

--- src/routes/profile/servers/+page.svelte
+++ src/routes/profile/servers/+page.svelte
@@ -14,16 +14,6 @@
 
 <main class="my-16 flex flex-col gap-16 lg:flex-row">
 	<section class="flex w-full max-w-3xl flex-col">
-		<h1 class="mb-4 text-3xl">Public Servers</h1>
-		{#if data.publicGuilds.length === 0}
-			<p>You're not a member of any public guilds!</p>
-		{/if}
-		<div class="mb-16 grid grid-flow-row-dense grid-cols-1 md:grid-cols-2">
-			{#each data.publicGuilds as guild (guild.id)}
-				<Guild {guild} link={true} />
-			{/each}
-		</div>
-
 		<h1 class="mb-4 text-2xl">Manage Servers</h1>
 		{#if data.adminGuilds.length === 0}
 			<p>You don't manage any servers with the bot invited!</p>
@@ -31,6 +21,16 @@
 		<div class="mb-16 grid grid-flow-row-dense grid-cols-1 md:grid-cols-2">
 			{#each data.adminGuilds as guild (guild.id)}
 				<Guild {guild} />
+			{/each}
+		</div>
+
+		<h1 class="mb-4 text-3xl">Public Servers</h1>
+		{#if data.publicGuilds.length === 0}
+			<p>You're not a member of any public guilds!</p>
+		{/if}
+		<div class="mb-16 grid grid-flow-row-dense grid-cols-1 md:grid-cols-2">
+			{#each data.publicGuilds as guild (guild.id)}
+				<Guild {guild} link={true} />
 			{/each}
 		</div>
 
