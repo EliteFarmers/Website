c0261b0da71451edf62005d9c2dfa79c70aa1728
Not a cow <kly3333aswd@gmail.com>
2025-01-16 18:57:06 -0800

    hardcode default theme options

--- src/components/header/mode-toggle.svelte
+++ src/components/header/mode-toggle.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script lang="ts" module>
 	import Check from 'lucide-svelte/icons/check';
 	import Moon from 'lucide-svelte/icons/moon';
 	import Sun from 'lucide-svelte/icons/sun';
@@ -7,14 +7,23 @@
 	import * as ModeWatcher from 'mode-watcher';
 	import { cn } from '$lib/utils';
 	import { themes } from '$lib/themes';
-	import { resetMode } from 'mode-watcher';
 	
 	let currentMode = $state<string | undefined>(undefined);
 	let currentTheme = $derived(themes.find(t => t.class === currentMode));
 
 	ModeWatcher.mode.subscribe((value) => currentMode = value);
 
-	const menuThemes = themes.slice(0, 2);
+	export function clearThemes() {
+		const themeClasses = themes.map(t => t.class);
+		document.documentElement.classList.remove(...themeClasses);
+		document.documentElement.className = '';
+		
+		const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+		const systemTheme = prefersDark ? 'dark' : 'light';
+		
+		document.documentElement.className = systemTheme;
+		currentMode = systemTheme;
+	}
 </script>
 
 <DropdownMenu.Root>
@@ -31,19 +40,29 @@
 		<span class="sr-only">Toggle theme</span>
 	</DropdownMenu.Trigger>
 	<DropdownMenu.Content align="end">
-		{#each menuThemes as theme}
-			<DropdownMenu.Item
-				onclick={() => {
-					ModeWatcher.setMode(theme.class);
-				}}
-			>
-				<span class="capitalize">{theme.name}</span>
-				{#if theme.class === currentMode}
-					<Check class="ml-2 h-4 w-4" />
-				{/if}
-			</DropdownMenu.Item>
-		{/each}
-		<DropdownMenu.Item onclick={() => resetMode()}>
+		<DropdownMenu.Item
+			onclick={() => {
+				document.documentElement.className = 'light';
+				currentMode = 'light';
+			}}
+		>
+			<span class="capitalize">Default Light</span>
+			{#if currentMode === 'light'}
+				<Check class="ml-2 h-4 w-4" />
+			{/if}
+		</DropdownMenu.Item>
+		<DropdownMenu.Item
+			onclick={() => {
+				document.documentElement.className = 'dark';
+				currentMode = 'dark';
+			}}
+		>
+			<span class="capitalize">Default Dark</span>
+			{#if currentMode === 'dark'}
+				<Check class="ml-2 h-4 w-4" />
+			{/if}
+		</DropdownMenu.Item>
+		<DropdownMenu.Item onclick={clearThemes}>
 			<span class="text-destructive">Use System Theme</span>
 		</DropdownMenu.Item>
 		<DropdownMenu.Separator />
