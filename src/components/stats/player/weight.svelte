59cc3087d802e8f8f9649e71c692492b8a1f3c27
Not a cow <kly3333aswd@gmail.com>
2025-02-12 09:10:35 -0800

    add popup

--- src/components/stats/player/weight.svelte
+++ src/components/stats/player/weight.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { getStatsContext } from '$lib/stores/stats.svelte';
+	import { toast } from 'svelte-sonner';
 	import Profiles from './profiles.svelte';
+	import { PUBLIC_WEIGHT_REQ } from '$env/static/public';
+	import { page } from '$app/state';
 
 	const ctx = getStatsContext();
 
@@ -8,6 +12,22 @@
 	const rank = $derived(ctx.ranks?.misc?.farmingweight);
 	const rankText = $derived(rank !== -1 ? `#${rank}` : 'Unranked');
 	const weightStr = $derived(weightInfo?.totalWeight?.toLocaleString() ?? '0');
+
+	onMount(() => {
+    const eligible = (ctx.member.farmingWeight?.totalWeight ?? 0) > Number(PUBLIC_WEIGHT_REQ);
+    const isOwnAccount = page.data.session.ign === ctx.ign
+    const hasElite = ctx.account.badges?.some((badge) => badge.id !== undefined && badge.id === 1) ?? false;
+
+    if (isOwnAccount && eligible && !hasElite) {
+			toast('You qualify for Elite!', {
+				description: 'Join the Discord and make a ticket to claim your role as an Elite Farmer',
+				action: {
+					label: 'x',
+					onClick: () => toast.dismiss()
+				}
+			});
+		}
+	});
 </script>
 
 <div class="block">
