a0edf13c681e39bd68cb041c312624fa5bd22310
Not a cow <kly3333aswd@gmail.com>
2025-04-18 09:48:01 -0700

    use tool.farmed

--- src/components/items/tools/farmingtools.svelte
+++ src/components/items/tools/farmingtools.svelte
@@ -27,14 +27,9 @@
 			};
 
 			actualTools = FT.fromArray(tools as EliteItemDto[], options).sort((a, b) => {
-				const aCounter =
-					a.counter !== undefined && a.counter !== null ? a.counter : Math.abs(a.cultivating ?? 0);
-				const bCounter =
-					b.counter !== undefined && b.counter !== null ? b.counter : Math.abs(b.cultivating ?? 0);
-
 				return (
-					calcWeightForCrop(b.crop ?? Crop.Wheat, bCounter) -
-					calcWeightForCrop(a.crop ?? Crop.Wheat, aCounter)
+					calcWeightForCrop(b.crop ?? Crop.Wheat, b.farmed) -
+					calcWeightForCrop(a.crop ?? Crop.Wheat, a.farmed)
 				);
 			});
 		}
