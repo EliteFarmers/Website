d6dc308efe8cd7c326771b023292f6365d0d3e60
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-05-11 21:50:35 -0400

    Make product cards look nicer on shop page

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -15,7 +15,7 @@
 <Head title="Shop" description="Help support development with cosmetics!" />
 
 <div class="mx-2 my-16 flex flex-col items-center gap-12">
-	<section class="flex max-w-4xl flex-col items-start gap-4">
+	<section class="flex max-w-6xl flex-col items-center gap-4">
 		<h1 class="mb-4 text-4xl">Elite Shop</h1>
 		<p class="max-w-2xl">
 			Welcome to the Elite Shop! Here you can purchase and preview cosmetics to show off your support for the
@@ -26,14 +26,14 @@
 			here before purchasing them in the store.
 		</p>
 		<!-- {#if data.styles?.[0]}
-            <WeightStyle style={data.styles?.[0]} />
-        {/if} -->
+				<WeightStyle style={data.styles?.[0]} />
+		{/if} -->
 	</section>
 
 	{#each categories as category (category.id)}
-		<section class="flex max-w-4xl flex-col items-start gap-4">
+		<section class="flex w-full max-w-[51rem] flex-col items-center gap-4 md:items-start">
 			<h2 class="text-2xl">{category.title}</h2>
-			<div class="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
+			<div class="flex flex-wrap items-start justify-center gap-4 md:justify-start">
 				{#each category.products ?? [] as product (product.id)}
 					<ProductCard {product} />
 				{/each}
@@ -41,9 +41,9 @@
 		</section>
 	{/each}
 
-	<section class="flex max-w-4xl flex-col items-start gap-4">
+	<section class="flex w-full max-w-[51rem] flex-col items-center gap-4 md:items-start">
 		<h2 class="text-2xl">All Products</h2>
-		<div class="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
+		<div class="flex flex-wrap items-start justify-center gap-4 md:justify-start">
 			{#each data.products as product (product.id)}
 				<ProductCard {product} />
 			{/each}
