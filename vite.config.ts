2b6c974ca75a141e3237a1e0d60396c41984950a
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-07-31 01:01:47 -0400

    Exclude napi-rs/canvas from bundle

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	optimizeDeps: {
+		exclude: ['@napi-rs/canvas'],
+	},
 	ssr: {
 		noExternal: process.env.NODE_ENV === 'production' ? ['apexcharts'] : [],
 	},
@@ -16,6 +19,21 @@ export default defineConfig({
 			$db: path.resolve('./src/database'),
 			$lib: path.resolve('./src/lib'),
 			$params: path.resolve('./src/params'),
+		},
+	},
+	build: {
+		sourcemap: true,
+		commonjsOptions: {
+			ignore: [
+				'@napi-rs/canvas-*',
+				'@napi-rs/canvas-darwin-arm64',
+				'@napi-rs/canvas-darwin-x64',
+				'@napi-rs/canvas-linux-arm64-gnu',
+				'@napi-rs/canvas-linux-arm64-musl',
+				'@napi-rs/canvas-linux-x64-gnu',
+				'@napi-rs/canvas-linux-x64-musl',
+				'@napi-rs/canvas-win32-x64-msvc',
+			],
 		},
 	},
 });
