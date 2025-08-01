baefa0e67c880e09699de4b19be02aed34af48ce
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-07-31 23:57:23 -0400

    Use default text outlines in name cards

--- src/components/stats/namecard/text-element.svelte
+++ src/components/stats/namecard/text-element.svelte
@@ -10,6 +10,8 @@
 	}
 
 	let { element, children, class: className, color }: Props = $props();
+
+	let outlineColor = $derived(element?.outline ? (element.outline.fill ?? '#000000') : 'transparent');
 </script>
 
 {#if color}
@@ -17,11 +19,7 @@
 		{@render children?.()}
 	</span>
 {:else}
-	<span
-		class={className}
-		style="color: {element?.fill ?? 'inherit'}; -webkit-text-stroke: 0.1rem {element?.outline?.fill ??
-			'transparent'};"
-	>
+	<span class={className} style="color: {element?.fill ?? 'inherit'}; -webkit-text-stroke: 0.1rem {outlineColor};">
 		{@render children?.()}
 	</span>
 {/if}
