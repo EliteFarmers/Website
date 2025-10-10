815f70808a685f1013dba68616b1b91b0347a3e5
Kaeso <24925519+ptlthg@users.noreply.github.com>
2025-10-09 21:57:52 -0400

    Fix skill bar loading state (#531)

--- src/components/stats/skills.svelte
+++ src/components/stats/skills.svelte
@@ -30,7 +30,7 @@
 					<div class="flex w-full flex-1 flex-col items-end justify-center gap-4 md:flex-row">
 						<Skillbar
 							name="Farming"
-							loading={ctx.ready}
+							loading={ctx.member.loading}
 							rank={ranks?.farming?.rank}
 							progress={getLevelProgress('farming', skills?.farming ?? 0, 50 + (levelCaps?.farming ?? 0))}
 						/>
@@ -40,7 +40,7 @@
 						</Button>
 						<Skillbar
 							name="Garden"
-							loading={ctx.ready}
+							loading={ctx.member.loading}
 							rank={ctx.ranks?.garden?.rank}
 							progress={getGardenLevel(gardenXp)}
 						/>
@@ -64,19 +64,19 @@
 							<Skillbar
 								name="Combat"
 								rank={ranks?.combat?.rank}
-								loading={ctx.ready}
+								loading={ctx.member.loading}
 								progress={getLevelProgress('combat', skills?.combat ?? 0)}
 							/>
 							<Skillbar
 								name="Mining"
-								loading={ctx.ready}
+								loading={ctx.member.loading}
 								rank={ranks?.mining?.rank}
 								progress={getLevelProgress('mining', skills?.mining ?? 0)}
 							/>
 							<Skillbar
 								name="Taming"
 								rank={ranks?.taming?.rank}
-								loading={ctx.ready}
+								loading={ctx.member.loading}
 								progress={getLevelProgress(
 									'taming',
 									skills?.taming ?? 0,
@@ -86,13 +86,13 @@
 							<Skillbar
 								name="Alchemy"
 								rank={ranks?.alchemy?.rank}
-								loading={ctx.ready}
+								loading={ctx.member.loading}
 								progress={getLevelProgress('alchemy', skills?.alchemy ?? 0)}
 							/>
 							<Skillbar
 								name="Runecrafting"
 								rank={ranks?.runecrafting?.rank}
-								loading={ctx.ready}
+								loading={ctx.member.loading}
 								progress={getLevelProgress('runecrafting', skills?.runecrafting ?? 0)}
 							/>
 						</div>
@@ -100,31 +100,31 @@
 							<Skillbar
 								name="Fishing"
 								rank={ranks?.fishing?.rank}
-								loading={ctx.ready}
+								loading={ctx.member.loading}
 								progress={getLevelProgress('fishing', skills?.fishing ?? 0)}
 							/>
 							<Skillbar
 								name="Foraging"
 								rank={ranks?.foraging?.rank}
-								loading={ctx.ready}
+								loading={ctx.member.loading}
 								progress={getLevelProgress('foraging', skills?.foraging ?? 0)}
 							/>
 							<Skillbar
 								name="Enchanting"
 								rank={ranks?.enchanting?.rank}
-								loading={ctx.ready}
+								loading={ctx.member.loading}
 								progress={getLevelProgress('enchanting', skills?.enchanting ?? 0)}
 							/>
 							<Skillbar
 								name="Carpentry"
 								rank={ranks?.carpentry?.rank}
-								loading={ctx.ready}
+								loading={ctx.member.loading}
 								progress={getLevelProgress('carpentry', skills?.carpentry ?? 0)}
 							/>
 							<Skillbar
 								name="Social"
 								rank={ranks?.social?.rank}
-								loading={ctx.ready}
+								loading={ctx.member.loading}
 								progress={getLevelProgress('social', skills?.social ?? 0)}
 							/>
 						</div>
