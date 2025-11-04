94cfc09744d8d4023c372e9d1866f88e7264bf26
Kaeso <24925519+ptlthg@users.noreply.github.com>
2025-11-04 15:37:07 -0500

    Fix siderail when sidebar is collapsed (#551)

--- src/routes/(main)/content.svelte
+++ src/routes/(main)/content.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import * as Sidebar from '$ui/sidebar';
-	import { SIDEBAR_WIDTH, SIDEBAR_WIDTH_ICON } from '$ui/sidebar/constants';
+	import { SIDEBAR_WIDTH } from '$ui/sidebar/constants';
 
 	interface Props {
 		children?: import('svelte').Snippet;
@@ -14,13 +14,13 @@
 
 <div class="@container">
 	<div
-		class="relative flex flex-col @7xl:flex-row @7xl:gap-4"
-		style="--sidebarWidth: {sidebarState.open ? SIDEBAR_WIDTH : SIDEBAR_WIDTH_ICON};"
+		class="relative flex flex-col {sidebarState.open ? '@7xl:flex-row' : ''} @7xl:gap-4"
+		style="--sidebarWidth: {SIDEBAR_WIDTH};"
 	>
 		<div class="mx-2 min-w-0 flex-1 md:mx-4">
 			{@render children?.()}
 		</div>
 
-		<div class="block @7xl:w-(--sidebarWidth)">{@render sidebar?.()}</div>
+		<div class="block {sidebarState.open ? '@7xl:w-(--sidebarWidth)' : ''}">{@render sidebar?.()}</div>
 	</div>
 </div>
