4cb668cba6e82a03465b76197221c024b65cbca5
Kaeso <24925519+ptlthg@users.noreply.github.com>
2026-02-27 22:56:52 -0500

    Add loading state to leaderboard page (#632)

--- src/routes/(main)/leaderboard/[category]/[[start=int]]/leaderboard-pagination.svelte
+++ src/routes/(main)/leaderboard/[category]/[[start=int]]/leaderboard-pagination.svelte
@@ -3,6 +3,7 @@
 	import type { LeaderboardDto } from '$lib/api';
 	import type { LeaderboardInfo } from '$lib/constants/leaderboards';
 	import { Button } from '$ui/button';
+	import { Skeleton } from '$ui/skeleton';
 	import ChevronLeft from '@lucide/svelte/icons/chevron-left';
 	import ChevronRight from '@lucide/svelte/icons/chevron-right';
 	import ChevronsLeft from '@lucide/svelte/icons/chevrons-left';
@@ -10,28 +11,33 @@
 
 	interface Props {
 		info: LeaderboardInfo;
-		leaderboard: LeaderboardDto;
+		leaderboard?: LeaderboardDto;
+		loading?: boolean;
 	}
 
-	let { info, leaderboard }: Props = $props();
+	let { info, leaderboard, loading = false }: Props = $props();
 
-	let currentPage = $derived(Math.floor(leaderboard.offset / 20) + 1);
-	let maxPage = $derived(Math.floor(leaderboard.maxEntries / 20) + 1);
+	let currentPage = $derived(leaderboard ? Math.floor(leaderboard.offset / 20) + 1 : 0);
+	let maxPage = $derived(leaderboard ? Math.floor(leaderboard.maxEntries / 20) + 1 : 0);
 </script>
 
 <div class="flex flex-col items-center gap-2 md:flex-row">
 	<div class="order-3 flex items-center justify-center text-sm font-medium whitespace-nowrap lg:order-1">
-		<span
-			>Page <strong>{currentPage.toLocaleString()}</strong> of
-			<strong>{maxPage.toLocaleString()}</strong></span
-		>
+		{#if loading}
+			<Skeleton class="h-6 w-24" />
+		{:else}
+			<span
+				>Page <strong>{currentPage.toLocaleString()}</strong> of
+				<strong>{maxPage.toLocaleString()}</strong></span
+			>
+		{/if}
 	</div>
 	<div class="order-2 flex items-center space-x-2">
 		<Button
 			variant="outline"
 			class="size-8 p-0"
 			href="/leaderboard/{info.id}{page.url.search}"
-			disabled={currentPage === 0}
+			disabled={currentPage === 0 || loading}
 		>
 			<span class="sr-only">Go to first page</span>
 			<ChevronsLeft />
@@ -39,8 +45,8 @@
 		<Button
 			variant="outline"
 			class="h-8 w-12 p-0"
-			href="/leaderboard/{info.id}/{Math.max(leaderboard.offset - 19, 1)}{page.url.search}"
-			disabled={currentPage === 0}
+			href="/leaderboard/{info.id}/{Math.max(leaderboard ? leaderboard.offset - 19 : 1, 1)}{page.url.search}"
+			disabled={currentPage === 0 || loading}
 		>
 			<span class="sr-only">Go to previous page</span>
 			<ChevronLeft />
@@ -48,8 +54,11 @@
 		<Button
 			variant="outline"
 			class="h-8 w-12 p-0"
-			href="/leaderboard/{info.id}/{Math.min(leaderboard.offset + 21, leaderboard.maxEntries)}{page.url.search}"
-			disabled={currentPage === maxPage}
+			href="/leaderboard/{info.id}/{Math.min(
+				leaderboard ? leaderboard.offset + 21 : 1,
+				leaderboard ? leaderboard.maxEntries : 1
+			)}{page.url.search}"
+			disabled={currentPage === maxPage || loading}
 		>
 			<span class="sr-only">Go to next page</span>
 			<ChevronRight />
@@ -58,7 +67,7 @@
 			variant="outline"
 			class="size-8 p-0"
 			href="/leaderboard/{info.id}/{Math.max(1, maxPage * 20 - 19)}{page.url.search}"
-			disabled={currentPage === maxPage}
+			disabled={currentPage === maxPage || loading}
 		>
 			<span class="sr-only">Go to last page</span>
 			<ChevronsRight />
