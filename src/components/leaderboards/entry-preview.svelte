78b94bc0e4bd33698e11e9e37ceb3dc2cc7f099c
ptlthg <24925519+ptlthg@users.noreply.github.com>
2026-08-21 21:44:48 -0400

    Oops

--- src/components/leaderboards/entry-preview.svelte
+++ src/components/leaderboards/entry-preview.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { LeaderboardScoreDataType, LeaderboardType, WeightStyleWithDataDto } from '$lib/api';
+	import type { WeightStyleWithDataDto } from '$lib/api';
 	import type { LeaderboardEntry } from '$lib/api/elite';
 	import type { LeaderboardStyle } from '$lib/styles/style';
 	import Entry from './entry.svelte';
